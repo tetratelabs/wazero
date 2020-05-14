6bd36975e51f7c798743076425295c3fe3018107
Andrea Manzini <ilmanzo@gmail.com>
2020-05-14 14:26:08 +0200

    add some tests

--- wasm/vm_num_test.go
+++ wasm/vm_num_test.go
@@ -1,3 +1,40 @@
 package wasm
 
-// fixme
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func buildvm() *VirtualMachine {
+	return &VirtualMachine{
+		OperandStack: NewVirtualMachineOperandStack(),
+	}
+}
+
+func Test_i32eqz(t *testing.T) {
+	vm := buildvm()
+	vm.OperandStack.Push(uint64(0))
+	i32eqz(vm)
+	assert.Equal(t, uint32(1), uint32(vm.OperandStack.Pop()))
+}
+
+func Test_i32eq(t *testing.T) {
+	vm := buildvm()
+	vm.OperandStack.Push(uint64(3))
+	vm.OperandStack.Push(uint64(3))
+	i32eq(vm)
+	assert.Equal(t, uint32(1), uint32(vm.OperandStack.Pop()))
+}
+
+func Test_i32ne(t *testing.T) {
+	vm := buildvm()
+	vm.OperandStack.Push(uint64(3))
+	vm.OperandStack.Push(uint64(4))
+	i32ne(vm)
+	assert.Equal(t, uint32(1), uint32(vm.OperandStack.Pop()))
+	vm.OperandStack.Push(uint64(3))
+	vm.OperandStack.Push(uint64(3))
+	i32ne(vm)
+	assert.Equal(t, uint32(0), uint32(vm.OperandStack.Pop()))
+}
