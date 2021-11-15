f83eaa3d0f9ee32a7819376d65cff0b4a95022a1
Takeshi Yoneda <takeshi@tetrate.io>
2021-11-15 12:11:52 +0900

    more
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- wasm/wazeroir/wazeroir.go
+++ wasm/wazeroir/wazeroir.go
@@ -4,9 +4,5 @@
 // in the previous codebase of wasmtime
 // e.g. https://github.com/bytecodealliance/wasmtime/blob/v0.29.0/crates/lightbeam/src/microwasm.rs
 //
-// The main difference from microwasm is that it doesn't have block operations
-// in order to simplify the implementation.
-// This package also has an implementation of direct interpreter of wazeroir which
-// implements the wasm.Engine interface, meaning that it can be used as an engine
-// of wazero runtime.
+// See RATIONALE.md for detail.
 package wazeroir
