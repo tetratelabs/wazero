f493238c83245e1efa2c362b48bf529f69fb2cb0
Takeshi Yoneda <takeshi@tetrate.io>
2021-11-22 16:34:48 +0900

    examples: panic on errors in Wasm. (#57)
    This commit makes sure that in-Wasm errors are detected as panics in examples.
    
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- examples/wasm/stdio.go
+++ examples/wasm/stdio.go
@@ -13,12 +13,10 @@ func main() {
 		line := s.Text()
 		if _, err := fmt.Printf("Hello, %s!\n", strings.TrimSpace(line)); err != nil {
 			panic(err)
-			return
 		}
 
 		if _, err := fmt.Fprintln(os.Stderr, "Error Message"); err != nil {
 			panic(err)
-			return
 		}
 	}
 }
