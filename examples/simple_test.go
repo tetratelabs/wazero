914df90133035a5644f7940ffa48e9e321764c98
Adrian Cole <adrian@tetrate.io>
2021-12-28 09:52:07 +0800

    tidies assertion
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- examples/simple_test.go
+++ examples/simple_test.go
@@ -1,6 +1,7 @@
 package examples
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"reflect"
@@ -26,22 +27,23 @@ func Test_Simple(t *testing.T) {
 
 	// Create a new store and add the function "hello" which the module imports
 	store := wasm.NewStore(wazeroir.NewEngine())
-	var called bool
-	hello := func(_ *wasm.HostFunctionCallContext) {
-		called = false
+
+	stdout := new(bytes.Buffer)
+	hostFunction := func(_ *wasm.HostFunctionCallContext) {
+		_, _ = fmt.Fprintln(stdout, "hello!")
 	}
-	
-	// ...
-	
-	require.True(t, called)
-	require.NoError(t, store.AddHostFunction("", "hello", reflect.ValueOf(hello)))
+
+	require.NoError(t, store.AddHostFunction("", "hello", reflect.ValueOf(hostFunction)))
 
 	// Now that the store has the prerequisite host function, instantiate the module.
 	moduleName := "simple"
 	require.NoError(t, store.Instantiate(mod, moduleName))
 
-	// Finally, invoke the function "run" which calls hello.
+	// Finally, invoke the function "run" which calls "hello".
 	// "run" has no parameters or result, so we can safely ignore all return values except the error.
 	_, _, err = store.CallFunction(moduleName, "run")
 	require.NoError(t, err)
+
+	// Ensure the host function "hello" was actually called!
+	require.Equal(t, "hello!\n", stdout.String())
 }
