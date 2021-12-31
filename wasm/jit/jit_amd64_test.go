6860cb4d9d4edd427f789ae955a71ac0902e8f6b
Takeshi Yoneda <takeshi@tetrate.io>
2021-12-31 09:11:32 +0900

    Update wasm/jit/jit_amd64_test.go
    Co-authored-by: Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>

--- wasm/jit/jit_amd64_test.go
+++ wasm/jit/jit_amd64_test.go
@@ -1412,7 +1412,7 @@ func TestAmd64Compiler_compileGe(t *testing.T) {
 			x1, x2 int64
 			signed bool
 		}{
-			{x1: 100, x2: -1, signed: false},
+			{x1: 100, x2: -1, signed: false}, // interpret x2 as max uint64
 			{x1: -1, x2: 100, signed: false},
 			{x1: 100, x2: 200, signed: true},
 			{x1: 200, x2: 100, signed: true},
