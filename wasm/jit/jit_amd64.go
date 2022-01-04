1f3b6d3b75ffac866d939862ab1a5a8a4d086781
Takeshi Yoneda <takeshi@tetrate.io>
2022-01-05 08:29:10 +0900

    Update wasm/jit/jit_amd64.go
    Co-authored-by: Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>

--- wasm/jit/jit_amd64.go
+++ wasm/jit/jit_amd64.go
@@ -1046,7 +1046,7 @@ func (c *amd64Compiler) compileCtz(o *wazeroir.OperationCtz) error {
 		c.addInstruction(countZeros)
 	} else {
 		// Somehow, if the target value is zero, TZCNT always returns zero: this is wrong.
-		// so we have the branches for non-zero and zero cases on macos.
+		// Meanwhile, we need branches for non-zero and zero cases on macos.
 		// TODO: find the reference to this behavior and put the link here.
 
 		// First we compare the target with zero.
