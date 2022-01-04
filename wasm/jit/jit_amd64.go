1f02cfd0e7ea95385428cb2e7353a1f49b477e60
Takeshi Yoneda <takeshi@tetrate.io>
2022-01-04 11:16:43 +0900

    a
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- wasm/jit/jit_amd64.go
+++ wasm/jit/jit_amd64.go
@@ -804,7 +804,7 @@ func (c *amd64Compiler) compileMul(o *wazeroir.OperationMul) (err error) {
 // multiplication on x86, see https://www.felixcloutier.com/x86/mul.
 // In summary, one of the values must be on the AX register,
 // and the mul instruction stores the overflow info in DX register which we don't use.
-// Here, we mean "the overflow info" by 65 bit or hight part of the result for 64 bit case.
+// Here, we mean "the overflow info" by 65 bit or higher part of the result for 64 bit case.
 // So, we have to ensure that
 // 1) Previously located value on DX must be saved to memory stack. That is because
 //    the existing value will be overriden after the mul execution.
