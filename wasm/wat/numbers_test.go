73c842748b572a41f8752f4698305f2ec78ebc64
Adrian Cole <adrian@tetrate.io>
2022-01-12 11:02:53 +0800

    finish coverage
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- wasm/wat/numbers_test.go
+++ wasm/wat/numbers_test.go
@@ -15,12 +15,15 @@ func TestDecodeUint32(t *testing.T) {
 	}{
 		{name: "zero", input: "0", expected: 0},
 		{name: "largest uint16", input: "65535", expected: 0xffff},
+		{name: "largest uint16 with underscores", input: "6_5_5_3_5", expected: 0xffff},
+		{name: "under largest uint32 by factor of 10", input: "429496729", expected: 429496729},
+		{name: "under largest uint32 by factor of 10 with underscores", input: "4_2_9_4_9_6_7_2_9", expected: 429496729},
 		{name: "largest uint32", input: "4294967295", expected: 0xffffffff},
 		{name: "largest uint32 with underscores", input: "4_2_9_4_9_6_7_2_9_5", expected: 0xffffffff},
 		{name: "overflow by one", input: "4294967296", expectedErr: true},
 		{name: "overflow by one with underscores", input: "4_2_9_4_9_6_7_2_9_6", expectedErr: true},
-		{name: "overflow by pow", input: "42949672950", expectedErr: true},
-		{name: "overflow by pow with underscores", input: "4_2_9_4_9_6_7_2_9_5_0", expectedErr: true},
+		{name: "overflow by factor of 10", input: "42949672950", expectedErr: true},
+		{name: "overflow by factor of 10 with underscores", input: "4_2_9_4_9_6_7_2_9_5_0", expectedErr: true},
 	} {
 		tc := tt
 		t.Run(tc.name, func(t *testing.T) {
@@ -42,14 +45,15 @@ func TestDecodeUint64(t *testing.T) {
 		expectedErr bool
 	}{
 		{name: "zero", input: "0", expected: 0},
-		{name: "largest uint16", input: "65535", expected: 0xffff},
 		{name: "largest uint32", input: "4294967295", expected: 0xffffffff},
+		{name: "under largest uint64 by factor of 10", input: "1844674407370955161", expected: 1844674407370955161},
+		{name: "under largest uint64 by factor of 10 with underscores", input: "1_8_4_4_6_7_4_4_0_7_3_7_0_9_5_5_1_6_1", expected: 1844674407370955161},
 		{name: "largest uint64", input: "18446744073709551615", expected: 0xffffffffffffffff},
 		{name: "largest uint64 with underscores", input: "1_8_4_4_6_7_4_4_0_7_3_7_0_9_5_5_1_6_1_5", expected: 0xffffffffffffffff},
 		{name: "overflow by one", input: "18446744073709551616", expectedErr: true},
 		{name: "overflow by one with underscores", input: "1_8_4_4_6_7_4_4_0_7_3_7_0_9_5_5_1_6_1_6", expectedErr: true},
-		{name: "overflow by pow", input: "184467440737095516150", expectedErr: true},
-		{name: "overflow by pow with underscores", input: "1_8_4_4_6_7_4_4_0_7_3_7_0_9_5_5_1_6_1_5_0", expectedErr: true},
+		{name: "overflow by factor of 10", input: "184467440737095516150", expectedErr: true},
+		{name: "overflow by factor of 10 with underscores", input: "1_8_4_4_6_7_4_4_0_7_3_7_0_9_5_5_1_6_1_5_0", expectedErr: true},
 	} {
 		tc := tt
 		t.Run(tc.name, func(t *testing.T) {
