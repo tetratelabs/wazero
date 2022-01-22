c8657d45de177c4028c57ecb89fca8767bfc9491
Takeshi Yoneda <takeshi@tetrate.io>
2022-01-22 09:15:43 +0900

    ok
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- wasm/jit/mmap_other.go
+++ wasm/jit/mmap_other.go
@@ -3,10 +3,6 @@
 
 package jit
 
-const mmapFlags = 0
-
-// Copy the code into the executable region
-// and returns the byte slice of the region.
 func mmapCodeSegment(code []byte) ([]byte, error) {
 	panic("unsupported GOOS")
 }
