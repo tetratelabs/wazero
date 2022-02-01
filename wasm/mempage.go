5d026cbe70f8122796927612191354df36761adb
Takeshi Yoneda <takeshi@tetrate.io>
2022-02-01 22:23:32 +0900

    Apply suggestions from code review
    Co-authored-by: Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>

--- wasm/mempage.go
+++ wasm/mempage.go
@@ -1,12 +1,12 @@
 package wasm
 
 const (
-	// memoryPageSize is the unit of memory length in WebAssembly,
+	// MemoryPageSize is the unit of memory length in WebAssembly,
 	// and is defined as 2^16 = 65536.
-	// https://www.w3.org/TR/wasm-core-1/#memory-instances%E2%91%A0
+	// See https://www.w3.org/TR/wasm-core-1/#memory-instances%E2%91%A0
 	MemoryPageSize = 65536
-	// The maximum number of pages is defined as 2^16.
-	// https://www.w3.org/TR/wasm-core-1/#grow-mem
+	// memoryMaxPages is maximum number of pages defined (2^16).
+	// See https://www.w3.org/TR/wasm-core-1/#grow-mem
 	memoryMaxPages = MemoryPageSize
 	// memoryPageSizeInBit satisfies the relation: "1 << memoryPageSizeInBit == memoryPageSize".
 	memoryPageSizeInBit = 16
