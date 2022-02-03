25fd77e7b652f9077125a13889540da7080b3b25
Takeshi Yoneda <takeshi@tetrate.io>
2022-02-03 14:19:50 +0900

    Return error on stack height limit exceed (#189)
    This commit implements static checks on the limit of maximum
    values on the stack a function can potentially reaches.
    This is the left TODO from #186.
    
    This also moves the validateFunction from store.go into func_validation.go

---
+++ wasm/func_validation_test.go
@@ -0,0 +1,38 @@
+package wasm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateFunction_valueStackLimit(t *testing.T) {
+	const max = 100
+	const valuesNum = max + 1
+
+	// Build a function which has max+1 const instructions.
+	f := &FunctionInstance{Body: []byte{}, FunctionType: &TypeInstance{Type: &FunctionType{}}}
+	for i := 0; i < valuesNum; i++ {
+		f.Body = append(f.Body, OpcodeI32Const, 1)
+	}
+
+	// Drop all the consts so that if the max is higher, this function body would be sound.
+	for i := 0; i < valuesNum; i++ {
+		f.Body = append(f.Body, OpcodeDrop)
+	}
+
+	// Plus all functions must end with End opcode.
+	f.Body = append(f.Body, OpcodeEnd)
+
+	t.Run("not exceed", func(t *testing.T) {
+		err := validateFunctionInstance(f, nil, nil, nil, nil, nil, max+1)
+		require.NoError(t, err)
+	})
+	t.Run("exceed", func(t *testing.T) {
+		err := validateFunctionInstance(f, nil, nil, nil, nil, nil, max)
+		require.Error(t, err)
+		expMsg := fmt.Sprintf("function may have %d stack values, which exceeds limit %d", valuesNum, max)
+		require.Equal(t, expMsg, err.Error())
+	})
+}
--- wasm/module_test.go
+++ wasm/module_test.go
@@ -1,38 +1,33 @@
 package wasm
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestFunctionType_String(t *testing.T) {
-	tp := FunctionType{}
-	require.Equal(t, "null_null", tp.String())
-
-	// With params.
-	tp = FunctionType{Params: []ValueType{ValueTypeI32}}
-	require.Equal(t, "i32_null", tp.String())
-	tp = FunctionType{Params: []ValueType{ValueTypeI32, ValueTypeF64}}
-	require.Equal(t, "i32f64_null", tp.String())
-	tp = FunctionType{Params: []ValueType{ValueTypeF32, ValueTypeI32, ValueTypeF64}}
-	require.Equal(t, "f32i32f64_null", tp.String())
-
-	// With results.
-	tp = FunctionType{Results: []ValueType{ValueTypeI64}}
-	require.Equal(t, "null_i64", tp.String())
-	tp = FunctionType{Results: []ValueType{ValueTypeI64, ValueTypeF32}}
-	require.Equal(t, "null_i64f32", tp.String())
-	tp = FunctionType{Results: []ValueType{ValueTypeF32, ValueTypeI32, ValueTypeF64}}
-	require.Equal(t, "null_f32i32f64", tp.String())
-
-	// With params and results.
-	tp = FunctionType{Params: []ValueType{ValueTypeI32}, Results: []ValueType{ValueTypeI64}}
-	require.Equal(t, "i32_i64", tp.String())
-	tp = FunctionType{Params: []ValueType{ValueTypeI64, ValueTypeF32}, Results: []ValueType{ValueTypeI64, ValueTypeF32}}
-	require.Equal(t, "i64f32_i64f32", tp.String())
-	tp = FunctionType{Params: []ValueType{ValueTypeI64, ValueTypeF32, ValueTypeF64}, Results: []ValueType{ValueTypeF32, ValueTypeI32, ValueTypeF64}}
-	require.Equal(t, "i64f32f64_f32i32f64", tp.String())
+	for _, tc := range []struct {
+		functype *FunctionType
+		exp      string
+	}{
+		{functype: &FunctionType{}, exp: "null_null"},
+		{functype: &FunctionType{Params: []ValueType{ValueTypeI32}}, exp: "i32_null"},
+		{functype: &FunctionType{Params: []ValueType{ValueTypeI32, ValueTypeF64}}, exp: "i32f64_null"},
+		{functype: &FunctionType{Params: []ValueType{ValueTypeF32, ValueTypeI32, ValueTypeF64}}, exp: "f32i32f64_null"},
+		{functype: &FunctionType{Results: []ValueType{ValueTypeI64}}, exp: "null_i64"},
+		{functype: &FunctionType{Results: []ValueType{ValueTypeI64, ValueTypeF32}}, exp: "null_i64f32"},
+		{functype: &FunctionType{Results: []ValueType{ValueTypeF32, ValueTypeI32, ValueTypeF64}}, exp: "null_f32i32f64"},
+		{functype: &FunctionType{Params: []ValueType{ValueTypeI32}, Results: []ValueType{ValueTypeI64}}, exp: "i32_i64"},
+		{functype: &FunctionType{Params: []ValueType{ValueTypeI64, ValueTypeF32}, Results: []ValueType{ValueTypeI64, ValueTypeF32}}, exp: "i64f32_i64f32"},
+		{functype: &FunctionType{Params: []ValueType{ValueTypeI64, ValueTypeF32, ValueTypeF64}, Results: []ValueType{ValueTypeF32, ValueTypeI32, ValueTypeF64}}, exp: "i64f32f64_f32i32f64"},
+	} {
+		tc := tc
+		t.Run(tc.functype.String(), func(t *testing.T) {
+			require.Equal(t, tc.exp, tc.functype.String())
+		})
+	}
 }
 
 func TestSectionIDName(t *testing.T) {
@@ -105,6 +100,106 @@ func TestValueTypeName(t *testing.T) {
 
 		t.Run(tc.name, func(t *testing.T) {
 			require.Equal(t, tc.expected, valueTypeName(tc.input))
+		})
+	}
+}
+
+func TestModule_allDeclarations(t *testing.T) {
+	for i, tc := range []struct {
+		module            *Module
+		expectedFunctions []Index
+		expectedGlobals   []*GlobalType
+		expectedMemories  []*MemoryType
+		expectedTables    []*TableType
+	}{
+		// Functions.
+		{
+			module: &Module{
+				ImportSection:   []*Import{{Kind: ImportKindFunc, DescFunc: 10000}},
+				FunctionSection: []Index{10, 20, 30},
+			},
+			expectedFunctions: []Index{10000, 10, 20, 30},
+		},
+		{
+			module: &Module{
+				FunctionSection: []Index{10, 20, 30},
+			},
+			expectedFunctions: []Index{10, 20, 30},
+		},
+		{
+			module: &Module{
+				ImportSection: []*Import{{Kind: ImportKindFunc, DescFunc: 10000}},
+			},
+			expectedFunctions: []Index{10000},
+		},
+		// Globals.
+		{
+			module: &Module{
+				ImportSection: []*Import{{Kind: ImportKindGlobal, DescGlobal: &GlobalType{Mutable: false}}},
+				GlobalSection: []*Global{{Type: &GlobalType{Mutable: true}}},
+			},
+			expectedGlobals: []*GlobalType{{Mutable: false}, {Mutable: true}},
+		},
+		{
+			module: &Module{
+				GlobalSection: []*Global{{Type: &GlobalType{Mutable: true}}},
+			},
+			expectedGlobals: []*GlobalType{{Mutable: true}},
+		},
+		{
+			module: &Module{
+				ImportSection: []*Import{{Kind: ImportKindGlobal, DescGlobal: &GlobalType{Mutable: false}}},
+			},
+			expectedGlobals: []*GlobalType{{Mutable: false}},
+		},
+		// Memories.
+		{
+			module: &Module{
+				ImportSection: []*Import{{Kind: ImportKindMemory, DescMem: &LimitsType{Min: 1}}},
+				MemorySection: []*MemoryType{{Min: 100}},
+			},
+			expectedMemories: []*MemoryType{{Min: 1}, {Min: 100}},
+		},
+		{
+			module: &Module{
+				ImportSection: []*Import{{Kind: ImportKindMemory, DescMem: &LimitsType{Min: 1}}},
+			},
+			expectedMemories: []*MemoryType{{Min: 1}},
+		},
+		{
+			module: &Module{
+				MemorySection: []*MemoryType{{Min: 100}},
+			},
+			expectedMemories: []*MemoryType{{Min: 100}},
+		},
+		// Tables.
+		{
+			module: &Module{
+				ImportSection: []*Import{{Kind: ImportKindTable, DescTable: &TableType{Limit: &LimitsType{Min: 1}}}},
+				TableSection:  []*TableType{{Limit: &LimitsType{Min: 10}}},
+			},
+			expectedTables: []*TableType{{Limit: &LimitsType{Min: 1}}, {Limit: &LimitsType{Min: 10}}},
+		},
+		{
+			module: &Module{
+				ImportSection: []*Import{{Kind: ImportKindTable, DescTable: &TableType{Limit: &LimitsType{Min: 1}}}},
+			},
+			expectedTables: []*TableType{{Limit: &LimitsType{Min: 1}}},
+		},
+		{
+			module: &Module{
+				TableSection: []*TableType{{Limit: &LimitsType{Min: 10}}},
+			},
+			expectedTables: []*TableType{{Limit: &LimitsType{Min: 10}}},
+		},
+	} {
+		tc := tc
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			functions, globals, memories, tables := tc.module.allDeclarations()
+			require.Equal(t, tc.expectedFunctions, functions)
+			require.Equal(t, tc.expectedGlobals, globals)
+			require.Equal(t, tc.expectedTables, tables)
+			require.Equal(t, tc.expectedMemories, memories)
 		})
 	}
 }
