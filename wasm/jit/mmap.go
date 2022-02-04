0cbb6d645fc9280f93ad57a3842c87112e7678ba
Takeshi Yoneda <takeshi@tetrate.io>
2022-02-04 16:40:12 +0900

    Fix mmap failure
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- wasm/jit/mmap.go
+++ wasm/jit/mmap.go
@@ -3,7 +3,9 @@
 
 package jit
 
-import "syscall"
+import (
+	"syscall"
+)
 
 // mmapCodeSegment copies the code into the executable region and returns the byte slice of the region.
 // See https://man7.org/linux/man-pages/man2/mmap.2.html for mmap API and flags.
@@ -12,8 +14,8 @@ func mmapCodeSegment(code []byte) ([]byte, error) {
 		-1,
 		0,
 		len(code),
-		// The region must be RWX: RW for writing native codes, X for executing the region.
-		syscall.PROT_READ|syscall.PROT_WRITE|syscall.PROT_EXEC,
+		// The region must be RW: RW for writing native codes.
+		syscall.PROT_READ|syscall.PROT_WRITE,
 		// Anonymous as this is not an actual file, but a memory,
 		// Private as this is in-process memory region.
 		syscall.MAP_ANON|syscall.MAP_PRIVATE,
@@ -22,5 +24,8 @@ func mmapCodeSegment(code []byte) ([]byte, error) {
 		return nil, err
 	}
 	copy(mmapFunc, code)
+
+	// Then we're done with writing code, change the permission to RX.
+	syscall.Mprotect(mmapFunc, syscall.PROT_READ|syscall.PROT_EXEC)
 	return mmapFunc, nil
 }
