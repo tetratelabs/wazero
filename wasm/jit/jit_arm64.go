5afc1fa78bb5dc8f4393cbbe9d3b637388f60298
Takeshi Yoneda <takeshi@tetrate.io>
2022-02-04 15:16:02 +0900

    fix typo
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- wasm/jit/jit_arm64.go
+++ wasm/jit/jit_arm64.go
@@ -19,297 +19,297 @@ func jitcall(codeSegment, engine uintptr)
 // newCompiler returns a new compiler interface which can be used to compile the given function instance.
 // Note: ir param can be nil for host functions.
 func newCompiler(f *wasm.FunctionInstance, ir *wazeroir.CompilationResult) (compiler, error) {
-	return &arm64Copmiler{}, nil
+	return &arm64Compiler{}, nil
 }
 
-type arm64Copmiler struct{}
+type arm64Compiler struct{}
 
-func (c *arm64Copmiler) String() (ret string) { return }
+func (c *arm64Compiler) String() (ret string) { return }
 
-func (c *arm64Copmiler) emitPreamble() error {
+func (c *arm64Compiler) emitPreamble() error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) generate() (code []byte, staticData compiledFunctionStaticData, maxStackPointer uint64, err error) {
+func (c *arm64Compiler) generate() (code []byte, staticData compiledFunctionStaticData, maxStackPointer uint64, err error) {
 	return nil, nil, 0, fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileHostFunction(address wasm.FunctionAddress) error {
+func (c *arm64Compiler) compileHostFunction(address wasm.FunctionAddress) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileLabel(o *wazeroir.OperationLabel) (skipThisLabel bool) {
+func (c *arm64Compiler) compileLabel(o *wazeroir.OperationLabel) (skipThisLabel bool) {
 	return false
 }
 
-func (c *arm64Copmiler) compileUnreachable() error {
+func (c *arm64Compiler) compileUnreachable() error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileSwap(o *wazeroir.OperationSwap) error {
+func (c *arm64Compiler) compileSwap(o *wazeroir.OperationSwap) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileGlobalGet(o *wazeroir.OperationGlobalGet) error {
+func (c *arm64Compiler) compileGlobalGet(o *wazeroir.OperationGlobalGet) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileGlobalSet(o *wazeroir.OperationGlobalSet) error {
+func (c *arm64Compiler) compileGlobalSet(o *wazeroir.OperationGlobalSet) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileBr(o *wazeroir.OperationBr) error {
+func (c *arm64Compiler) compileBr(o *wazeroir.OperationBr) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileBrIf(o *wazeroir.OperationBrIf) error {
+func (c *arm64Compiler) compileBrIf(o *wazeroir.OperationBrIf) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileBrTable(o *wazeroir.OperationBrTable) error {
+func (c *arm64Compiler) compileBrTable(o *wazeroir.OperationBrTable) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileCall(o *wazeroir.OperationCall) error {
+func (c *arm64Compiler) compileCall(o *wazeroir.OperationCall) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileCallIndirect(o *wazeroir.OperationCallIndirect) error {
+func (c *arm64Compiler) compileCallIndirect(o *wazeroir.OperationCallIndirect) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileDrop(o *wazeroir.OperationDrop) error {
+func (c *arm64Compiler) compileDrop(o *wazeroir.OperationDrop) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileSelect() error {
+func (c *arm64Compiler) compileSelect() error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compilePick(o *wazeroir.OperationPick) error {
+func (c *arm64Compiler) compilePick(o *wazeroir.OperationPick) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileAdd(o *wazeroir.OperationAdd) error {
+func (c *arm64Compiler) compileAdd(o *wazeroir.OperationAdd) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileSub(o *wazeroir.OperationSub) error {
+func (c *arm64Compiler) compileSub(o *wazeroir.OperationSub) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileMul(o *wazeroir.OperationMul) error {
+func (c *arm64Compiler) compileMul(o *wazeroir.OperationMul) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileClz(o *wazeroir.OperationClz) error {
+func (c *arm64Compiler) compileClz(o *wazeroir.OperationClz) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileCtz(o *wazeroir.OperationCtz) error {
+func (c *arm64Compiler) compileCtz(o *wazeroir.OperationCtz) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compilePopcnt(o *wazeroir.OperationPopcnt) error {
+func (c *arm64Compiler) compilePopcnt(o *wazeroir.OperationPopcnt) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileDiv(o *wazeroir.OperationDiv) error {
+func (c *arm64Compiler) compileDiv(o *wazeroir.OperationDiv) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileRem(o *wazeroir.OperationRem) error {
+func (c *arm64Compiler) compileRem(o *wazeroir.OperationRem) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileAnd(o *wazeroir.OperationAnd) error {
+func (c *arm64Compiler) compileAnd(o *wazeroir.OperationAnd) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileOr(o *wazeroir.OperationOr) error {
+func (c *arm64Compiler) compileOr(o *wazeroir.OperationOr) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileXor(o *wazeroir.OperationXor) error {
+func (c *arm64Compiler) compileXor(o *wazeroir.OperationXor) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileShl(o *wazeroir.OperationShl) error {
+func (c *arm64Compiler) compileShl(o *wazeroir.OperationShl) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileShr(o *wazeroir.OperationShr) error {
+func (c *arm64Compiler) compileShr(o *wazeroir.OperationShr) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileRotl(o *wazeroir.OperationRotl) error {
+func (c *arm64Compiler) compileRotl(o *wazeroir.OperationRotl) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileRotr(o *wazeroir.OperationRotr) error {
+func (c *arm64Compiler) compileRotr(o *wazeroir.OperationRotr) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileAbs(o *wazeroir.OperationAbs) error {
+func (c *arm64Compiler) compileAbs(o *wazeroir.OperationAbs) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileNeg(o *wazeroir.OperationNeg) error {
+func (c *arm64Compiler) compileNeg(o *wazeroir.OperationNeg) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileCeil(o *wazeroir.OperationCeil) error {
+func (c *arm64Compiler) compileCeil(o *wazeroir.OperationCeil) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileFloor(o *wazeroir.OperationFloor) error {
+func (c *arm64Compiler) compileFloor(o *wazeroir.OperationFloor) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileTrunc(o *wazeroir.OperationTrunc) error {
+func (c *arm64Compiler) compileTrunc(o *wazeroir.OperationTrunc) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileNearest(o *wazeroir.OperationNearest) error {
+func (c *arm64Compiler) compileNearest(o *wazeroir.OperationNearest) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileSqrt(o *wazeroir.OperationSqrt) error {
+func (c *arm64Compiler) compileSqrt(o *wazeroir.OperationSqrt) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileMin(o *wazeroir.OperationMin) error {
+func (c *arm64Compiler) compileMin(o *wazeroir.OperationMin) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileMax(o *wazeroir.OperationMax) error {
+func (c *arm64Compiler) compileMax(o *wazeroir.OperationMax) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileCopysign(o *wazeroir.OperationCopysign) error {
+func (c *arm64Compiler) compileCopysign(o *wazeroir.OperationCopysign) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileI32WrapFromI64() error {
+func (c *arm64Compiler) compileI32WrapFromI64() error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileITruncFromF(o *wazeroir.OperationITruncFromF) error {
+func (c *arm64Compiler) compileITruncFromF(o *wazeroir.OperationITruncFromF) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileFConvertFromI(o *wazeroir.OperationFConvertFromI) error {
+func (c *arm64Compiler) compileFConvertFromI(o *wazeroir.OperationFConvertFromI) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileF32DemoteFromF64() error {
+func (c *arm64Compiler) compileF32DemoteFromF64() error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileF64PromoteFromF32() error {
+func (c *arm64Compiler) compileF64PromoteFromF32() error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileI32ReinterpretFromF32() error {
+func (c *arm64Compiler) compileI32ReinterpretFromF32() error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileI64ReinterpretFromF64() error {
+func (c *arm64Compiler) compileI64ReinterpretFromF64() error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileF32ReinterpretFromI32() error {
+func (c *arm64Compiler) compileF32ReinterpretFromI32() error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileF64ReinterpretFromI64() error {
+func (c *arm64Compiler) compileF64ReinterpretFromI64() error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileExtend(o *wazeroir.OperationExtend) error {
+func (c *arm64Compiler) compileExtend(o *wazeroir.OperationExtend) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileEq(o *wazeroir.OperationEq) error {
+func (c *arm64Compiler) compileEq(o *wazeroir.OperationEq) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileNe(o *wazeroir.OperationNe) error {
+func (c *arm64Compiler) compileNe(o *wazeroir.OperationNe) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileEqz(o *wazeroir.OperationEqz) error {
+func (c *arm64Compiler) compileEqz(o *wazeroir.OperationEqz) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileLt(o *wazeroir.OperationLt) error {
+func (c *arm64Compiler) compileLt(o *wazeroir.OperationLt) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileGt(o *wazeroir.OperationGt) error {
+func (c *arm64Compiler) compileGt(o *wazeroir.OperationGt) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileLe(o *wazeroir.OperationLe) error {
+func (c *arm64Compiler) compileLe(o *wazeroir.OperationLe) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileGe(o *wazeroir.OperationGe) error {
+func (c *arm64Compiler) compileGe(o *wazeroir.OperationGe) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileLoad(o *wazeroir.OperationLoad) error {
+func (c *arm64Compiler) compileLoad(o *wazeroir.OperationLoad) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileLoad8(o *wazeroir.OperationLoad8) error {
+func (c *arm64Compiler) compileLoad8(o *wazeroir.OperationLoad8) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileLoad16(o *wazeroir.OperationLoad16) error {
+func (c *arm64Compiler) compileLoad16(o *wazeroir.OperationLoad16) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileLoad32(o *wazeroir.OperationLoad32) error {
+func (c *arm64Compiler) compileLoad32(o *wazeroir.OperationLoad32) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileStore(o *wazeroir.OperationStore) error {
+func (c *arm64Compiler) compileStore(o *wazeroir.OperationStore) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileStore8(o *wazeroir.OperationStore8) error {
+func (c *arm64Compiler) compileStore8(o *wazeroir.OperationStore8) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileStore16(o *wazeroir.OperationStore16) error {
+func (c *arm64Compiler) compileStore16(o *wazeroir.OperationStore16) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileStore32(o *wazeroir.OperationStore32) error {
+func (c *arm64Compiler) compileStore32(o *wazeroir.OperationStore32) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileMemoryGrow() error {
+func (c *arm64Compiler) compileMemoryGrow() error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileMemorySize() error {
+func (c *arm64Compiler) compileMemorySize() error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileConstI32(o *wazeroir.OperationConstI32) error {
+func (c *arm64Compiler) compileConstI32(o *wazeroir.OperationConstI32) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileConstI64(o *wazeroir.OperationConstI64) error {
+func (c *arm64Compiler) compileConstI64(o *wazeroir.OperationConstI64) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileConstF32(o *wazeroir.OperationConstF32) error {
+func (c *arm64Compiler) compileConstF32(o *wazeroir.OperationConstF32) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
 
-func (c *arm64Copmiler) compileConstF64(o *wazeroir.OperationConstF64) error {
+func (c *arm64Compiler) compileConstF64(o *wazeroir.OperationConstF64) error {
 	return fmt.Errorf("TODO: unsupported on arm64")
 }
