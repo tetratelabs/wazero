584f3739aba8d61532a54561740858a6db4da608
Takeshi Yoneda <takeshi@tetrate.io>
2022-03-14 09:47:24 +0900

    add test
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- tests/engine/concurrency_test.go
+++ tests/engine/concurrency_test.go
@@ -57,10 +57,15 @@ func runAdhocTestUnderHighConcurrency(t *testing.T, newRuntimeConfig func() *waz
 }
 
 func singleModuleHighConcurrency(t *testing.T, newRuntimeConfig func() *wazero.RuntimeConfig) {
+	args := []uint64{1, 123}
+	exp := args[0] + args[1]
+	const goroutines = 1000
+	const delay = 100 * time.Millisecond
+
 	t.Run("single module", func(t *testing.T) {
 		r := wazero.NewRuntimeWithConfig(newRuntimeConfig())
 		imported, err := r.NewModuleBuilder("host").ExportFunctions(map[string]interface{}{
-			"delay": func() { time.Sleep(100 * time.Millisecond) },
+			"delay": func() { time.Sleep(delay) },
 		}).Instantiate()
 		require.NoError(t, err)
 
@@ -80,15 +85,11 @@ func singleModuleHighConcurrency(t *testing.T, newRuntimeConfig func() *wazero.R
 		module, err := r.InstantiateModuleFromSource(source)
 		require.NoError(t, err)
 
-		args := []uint64{1, 123}
-		exp := args[0] + args[1]
-
 		t.Run("close importing module while in use", func(t *testing.T) {
 			fn := module.ExportedFunction("delay_add")
 			require.NotNil(t, fn)
 
 			var wg sync.WaitGroup
-			const goroutines = 1000
 			wg.Add(goroutines)
 			for i := 0; i < goroutines; i++ {
 				if i == 200 {
@@ -116,7 +117,6 @@ func singleModuleHighConcurrency(t *testing.T, newRuntimeConfig func() *wazero.R
 
 			var newImportedModuleShouldPanic bool = true
 			var wg sync.WaitGroup
-			const goroutines = 1000
 			wg.Add(goroutines)
 			for i := 0; i < goroutines; i++ {
 				if i == 200 {
@@ -126,16 +126,16 @@ func singleModuleHighConcurrency(t *testing.T, newRuntimeConfig func() *wazero.R
 					// not use the new one.
 					imported, err = r.NewModuleBuilder("host").ExportFunctions(map[string]interface{}{
 						"delay": func() {
+							time.Sleep(delay)
 							if newImportedModuleShouldPanic {
 								panic("unreachable")
-							} else {
-								time.Sleep(10 * time.Millisecond)
 							}
 						},
 					}).Instantiate()
 					require.NoError(t, err)
 				} else if i == 400 {
-					// Re-instantiate the importing module, and swap the function.
+					// Re-instantiate the importing module, and swap the function which will use
+					// the new imported module.
 					module.Close()
 					module, err = r.InstantiateModuleFromSource(source)
 					require.NoError(t, err)
