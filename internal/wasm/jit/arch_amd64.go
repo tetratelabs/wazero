c3c0c51d7c8869c913defe946afcc32a1ecb4f91
Takeshi Yoneda <takeshi@tetrate.io>
2022-03-18 10:24:54 +0900

    more
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- internal/wasm/jit/arch_amd64.go
+++ internal/wasm/jit/arch_amd64.go
@@ -1,12 +1,5 @@
 package jit
 
-import (
-	wasm "github.com/tetratelabs/wazero/internal/wasm"
-	"github.com/tetratelabs/wazero/internal/wasm/jit/asm"
-	"github.com/tetratelabs/wazero/internal/wasm/jit/asm/amd64"
-	"github.com/tetratelabs/wazero/internal/wazeroir"
-)
-
 // init initializes variables for the amd64 architecture
 func init() {
 	newArchContext = newArchContextImpl
