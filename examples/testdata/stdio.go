5b35bd9b66176b064a122d348a71e6f02daeb6b2
Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>
2022-03-19 20:01:55 +0800

    Makes filesystem test simpler and removes use of panic in examples (#395)
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- examples/testdata/stdio.go
+++ examples/testdata/stdio.go
@@ -12,11 +12,11 @@ func main() {
 	for s.Scan() {
 		line := s.Text()
 		if _, err := fmt.Printf("Hello, %s!\n", strings.TrimSpace(line)); err != nil {
-			panic(err)
+			os.Exit(1)
 		}
 
 		if _, err := fmt.Fprintln(os.Stderr, "Error Message"); err != nil {
-			panic(err)
+			os.Exit(1)
 		}
 	}
 }
