18595764d7241cf3bc068d1d60876cf06ca030a6
Takeshi Yoneda <takeshi@tetrate.io>
2022-04-21 11:11:56 +0900

    done interpreter
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- internal/wasm/interpreter/interpreter_test.go
+++ internal/wasm/interpreter/interpreter_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"math"
+	"strconv"
 	"testing"
 
 	"github.com/tetratelabs/wazero/internal/buildoptions"
@@ -86,6 +87,201 @@ func TestInterpreter_ModuleEngine_Call_HostFn(t *testing.T) {
 
 func TestInterpreter_ModuleEngine_Call_Errors(t *testing.T) {
 	enginetest.RunTestModuleEngine_Call_Errors(t, et)
+}
+
+func TestInterpreter_NonTrappingFloatToIntConversion(t *testing.T) {
+	_0x80000000 := uint32(0x80000000)
+	_0xffffffff := uint32(0xffffffff)
+	_0x8000000000000000 := uint64(0x8000000000000000)
+	_0xffffffffffffffff := uint64(0xffffffffffffffff)
+
+	for _, tc := range []struct {
+		op            wasm.OpcodeMisc
+		inputType     wazeroir.Float
+		outputType    wazeroir.SignedInt
+		input32bit    []float32
+		input64bit    []float64
+		expected32bit []int32
+		expected64bit []int64
+	}{
+		{
+			// https://github.com/WebAssembly/spec/blob/c8fd933fa51eb0b511bce027b573aef7ee373726/test/core/conversions.wast#L261-L282
+			op:         wasm.OpcodeMiscI32TruncSatF32S,
+			inputType:  wazeroir.Float32,
+			outputType: wazeroir.SignedInt32,
+			input32bit: []float32{
+				0.0, 0.0, 0x1p-149, -0x1p-149, 1.0, 0x1.19999ap+0, 1.5, -1.0, -0x1.19999ap+0,
+				-1.5, -1.9, -2.0, 2147483520.0, -2147483648.0, 2147483648.0, -2147483904.0,
+				float32(math.Inf(1)), float32(math.Inf(-1)), float32(math.NaN()), float32(math.NaN()),
+				float32(math.NaN()), float32(math.NaN()),
+			},
+			expected32bit: []int32{
+				0, 0, 0, 0, 1, 1, 1, -1, -1, -1, -1, -2, 2147483520, -2147483648, 0x7fffffff,
+				int32(_0x80000000), 0x7fffffff, int32(_0x80000000), 0, 0, 0, 0,
+			},
+		},
+		{
+			// https://github.com/WebAssembly/spec/blob/c8fd933fa51eb0b511bce027b573aef7ee373726/test/core/conversions.wast#L284-L304
+			op:         wasm.OpcodeMiscI32TruncSatF32U,
+			inputType:  wazeroir.Float32,
+			outputType: wazeroir.SignedUint32,
+			input32bit: []float32{
+				0.0, 0.0, 0x1p-149, -0x1p-149, 1.0, 0x1.19999ap+0, 1.5, 1.9, 2.0, 2147483648, 4294967040.0,
+				-0x1.ccccccp-1, -0x1.fffffep-1, 4294967296.0, -1.0, float32(math.Inf(1)), float32(math.Inf(-1)),
+				float32(math.NaN()), float32(math.NaN()), float32(math.NaN()), float32(math.NaN()),
+			},
+			expected32bit: []int32{
+				0, 0, 0, 0, 1, 1, 1, 1, 2, -2147483648, -256, 0, 0, int32(_0xffffffff), 0x00000000,
+				int32(_0xffffffff), 0x00000000, 0, 0, 0, 0,
+			},
+		},
+		{
+			// https://github.com/WebAssembly/spec/blob/c8fd933fa51eb0b511bce027b573aef7ee373726/test/core/conversions.wast#L355-L378
+			op:         wasm.OpcodeMiscI64TruncSatF32S,
+			inputType:  wazeroir.Float32,
+			outputType: wazeroir.SignedInt64,
+			input32bit: []float32{
+				0.0, 0.0, 0x1p-149, -0x1p-149, 1.0, 0x1.19999ap+0, 1.5, -1.0, -0x1.19999ap+0, -1.5, -1.9, -2.0, 4294967296,
+				-4294967296, 9223371487098961920.0, -9223372036854775808.0, 9223372036854775808.0, -9223373136366403584.0,
+				float32(math.Inf(1)), float32(math.Inf(-1)), float32(math.NaN()), float32(math.NaN()), float32(math.NaN()),
+				float32(math.NaN()),
+			},
+			expected64bit: []int64{
+				0, 0, 0, 0, 1, 1, 1, -1, -1, -1, -1, -2, 4294967296, -4294967296, 9223371487098961920, -9223372036854775808,
+				0x7fffffffffffffff, int64(_0x8000000000000000), 0x7fffffffffffffff, int64(_0x8000000000000000), 0, 0, 0, 0,
+			},
+		},
+		{
+			// https://github.com/WebAssembly/spec/blob/c8fd933fa51eb0b511bce027b573aef7ee373726/test/core/conversions.wast#L380-L398
+			op:         wasm.OpcodeMiscI64TruncSatF32U,
+			inputType:  wazeroir.Float32,
+			outputType: wazeroir.SignedUint64,
+			input32bit: []float32{
+				0.0, 0.0, 0x1p-149, -0x1p-149, 1.0, 0x1.19999ap+0, 1.5, 4294967296,
+				18446742974197923840.0, -0x1.ccccccp-1, -0x1.fffffep-1, 18446744073709551616.0, -1.0,
+				float32(math.Inf(1)), float32(math.Inf(-1)), float32(math.NaN()), float32(math.NaN()),
+				float32(math.NaN()), float32(math.NaN()),
+			},
+			expected64bit: []int64{
+				0, 0, 0, 0, 1, 1, 1,
+				4294967296, -1099511627776, 0, 0, int64(_0xffffffffffffffff), 0x0000000000000000,
+				int64(_0xffffffffffffffff), 0x0000000000000000, 0, 0, 0, 0,
+			},
+		},
+		{
+			// https://github.com/WebAssembly/spec/blob/c8fd933fa51eb0b511bce027b573aef7ee373726/test/core/conversions.wast#L306-L327
+			op:         wasm.OpcodeMiscI32TruncSatF64S,
+			inputType:  wazeroir.Float64,
+			outputType: wazeroir.SignedInt32,
+			input64bit: []float64{
+				0.0, 0.0, 0x0.0000000000001p-1022, -0x0.0000000000001p-1022, 1.0, 0x1.199999999999ap+0, 1.5, -1.0,
+				-0x1.199999999999ap+0, -1.5, -1.9, -2.0, 2147483647.0, -2147483648.0, 2147483648.0,
+				-2147483649.0, math.Inf(1), math.Inf(-1), math.NaN(), math.NaN(), math.NaN(), math.NaN(),
+			},
+			expected32bit: []int32{
+				0, 0, 0, 0, 1, 1, 1, -1, -1, -1, -1, -2,
+				2147483647, -2147483648, 0x7fffffff, int32(_0x80000000), 0x7fffffff, int32(_0x80000000), 0,
+				0, 0, 0,
+			},
+		},
+		{
+			// https://github.com/WebAssembly/spec/blob/c8fd933fa51eb0b511bce027b573aef7ee373726/test/core/conversions.wast#L329-L353
+			op:         wasm.OpcodeMiscI32TruncSatF64U,
+			inputType:  wazeroir.Float64,
+			outputType: wazeroir.SignedUint32,
+			input64bit: []float64{
+				0.0, 0.0, 0x0.0000000000001p-1022, -0x0.0000000000001p-1022, 1.0, 0x1.199999999999ap+0, 1.5, 1.9, 2.0,
+				2147483648, 4294967295.0, -0x1.ccccccccccccdp-1, -0x1.fffffffffffffp-1, 1e8, 4294967296.0, -1.0, 1e16, 1e30,
+				9223372036854775808, math.Inf(1), math.Inf(-1), math.NaN(), math.NaN(), math.NaN(), math.NaN(),
+			},
+			expected32bit: []int32{
+				0, 0, 0, 0, 1, 1, 1, 1, 2, -2147483648, -1,
+				0, 0, 100000000, int32(_0xffffffff), 0x00000000, int32(_0xffffffff), int32(_0xffffffff), int32(_0xffffffff),
+				int32(_0xffffffff), 0x00000000, 0, 0, 0, 0,
+			},
+		},
+		{
+			// https://github.com/WebAssembly/spec/blob/c8fd933fa51eb0b511bce027b573aef7ee373726/test/core/conversions.wast#L400-L423
+			op:         wasm.OpcodeMiscI64TruncSatF64S,
+			inputType:  wazeroir.Float64,
+			outputType: wazeroir.SignedInt64,
+			input64bit: []float64{
+				0.0, 0.0, 0x0.0000000000001p-1022, -0x0.0000000000001p-1022, 1.0, 0x1.199999999999ap+0, 1.5, -1.0,
+				-0x1.199999999999ap+0, -1.5, -1.9, -2.0, 4294967296, -4294967296, 9223372036854774784.0, -9223372036854775808.0,
+				9223372036854775808.0, -9223372036854777856.0, math.Inf(1), math.Inf(-1), math.NaN(), math.NaN(), math.NaN(),
+				math.NaN(),
+			},
+			expected64bit: []int64{
+				0, 0, 0, 0, 1, 1, 1, -1, -1, -1, -1, -2,
+				4294967296, -4294967296, 9223372036854774784, -9223372036854775808, 0x7fffffffffffffff,
+				int64(_0x8000000000000000), 0x7fffffffffffffff, int64(_0x8000000000000000), 0, 0, 0, 0,
+			},
+		},
+		{
+			// https://github.com/WebAssembly/spec/blob/c8fd933fa51eb0b511bce027b573aef7ee373726/test/core/conversions.wast#L425-L447
+			op:         wasm.OpcodeMiscI64TruncSatF64U,
+			inputType:  wazeroir.Float64,
+			outputType: wazeroir.SignedUint64,
+			input64bit: []float64{
+				0.0, 0.0, 0x0.0000000000001p-1022, -0x0.0000000000001p-1022, 1.0, 0x1.199999999999ap+0, 1.5, 4294967295, 4294967296,
+				18446744073709549568.0, -0x1.ccccccccccccdp-1, -0x1.fffffffffffffp-1, 1e8, 1e16, 9223372036854775808,
+				18446744073709551616.0, -1.0, math.Inf(1), math.Inf(-1), math.NaN(), math.NaN(), math.NaN(), math.NaN(),
+			},
+			expected64bit: []int64{
+				0, 0, 0, 0, 1, 1, 1, 0xffffffff, 0x100000000, -2048, 0, 0, 100000000, 10000000000000000,
+				-9223372036854775808, int64(_0xffffffffffffffff), 0x0000000000000000, int64(_0xffffffffffffffff),
+				0x0000000000000000, 0, 0, 0, 0,
+			},
+		},
+	} {
+		tc := tc
+		t.Run(wasm.MiscInstructionName(tc.op), func(t *testing.T) {
+			in32bit := len(tc.input32bit) > 0
+			casenum := len(tc.input32bit)
+			if !in32bit {
+				casenum = len(tc.input64bit)
+			}
+			for i := 0; i < casenum; i++ {
+				i := i
+				t.Run(strconv.Itoa(i), func(t *testing.T) {
+					var body []*interpreterOp
+					if in32bit {
+						body = append(body, &interpreterOp{kind: wazeroir.OperationKindConstF32,
+							us: []uint64{uint64(math.Float32bits(tc.input32bit[i]))}})
+					} else {
+						body = append(body, &interpreterOp{kind: wazeroir.OperationKindConstF64,
+							us: []uint64{uint64(math.Float64bits(tc.input64bit[i]))}})
+					}
+
+					body = append(body, &interpreterOp{
+						kind: wazeroir.OperationKindITruncFromF,
+						b1:   byte(tc.inputType),
+						b2:   byte(tc.outputType),
+						b3:   true, // NonTrapping = true.
+					})
+
+					// Return from function.
+					body = append(body,
+						&interpreterOp{kind: wazeroir.OperationKindBr, us: []uint64{math.MaxUint64}},
+					)
+
+					ce := &callEngine{}
+					f := &function{
+						source: &wasm.FunctionInstance{Module: &wasm.ModuleInstance{Engine: &moduleEngine{}}},
+						body:   body,
+					}
+					ce.callNativeFunc(testCtx, &wasm.CallContext{}, f)
+
+					if len(tc.expected32bit) > 0 {
+						require.Equal(t, tc.expected32bit[i], int32(uint32(ce.popValue())))
+					} else {
+						require.Equal(t, tc.expected64bit[i], int64((ce.popValue())))
+					}
+				})
+			}
+		})
+
+	}
 }
 
 func TestInterpreter_CallEngine_callNativeFunc_signExtend(t *testing.T) {
