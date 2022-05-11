b6114853fce1fc656a842251da5dabb5a1a6f8f7
Takeshi Yoneda <takeshi@tetrate.io>
2022-05-11 13:55:38 +0900

    lint
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- internal/integration_test/spectest/v2/spec_test.go
+++ internal/integration_test/spectest/v2/spec_test.go
@@ -2,7 +2,6 @@ package spectest
 
 import (
 	"embed"
-	"runtime"
 	"testing"
 
 	"github.com/tetratelabs/wazero/internal/integration_test/spectest"
@@ -13,13 +12,13 @@ import (
 
 //go:embed testdata/*.wasm
 //go:embed testdata/*.json
-var testcases embed.FS
+var testcases embed.FS //nolint:unused
 
 const enabledFeatures = wasm.Features20220419
 
 func TestJIT(t *testing.T) {
 	t.Skip() // TODO!
-	if runtime.GOARCH != "amd64" && runtime.GOARCH != "arm64" {
+	if true {
 		t.Skip()
 	}
 
