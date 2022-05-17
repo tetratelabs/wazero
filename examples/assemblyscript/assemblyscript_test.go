743a8dddaf4ecf03e55f401020dded9e4ede562e
Anuraag Agrawal <anuraaga@gmail.com>
2022-05-17 15:29:11 +0900

    Skip test on riscv64
    Signed-off-by: Anuraag Agrawal <anuraaga@gmail.com>

--- examples/assemblyscript/assemblyscript_test.go
+++ examples/assemblyscript/assemblyscript_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/tetratelabs/wazero/internal/testing/maintester"
@@ -11,6 +12,9 @@ import (
 //
 // go run assemblyscript.go 7
 func Test_main(t *testing.T) {
+	if runtime.GOARCH == "riscv64" {
+		t.Skip("Uses bulk memory operations which are not implemented on riscv64 yet.")
+	}
 	stdout, stderr := maintester.TestMain(t, main, "assemblyscript", "7")
 	require.Equal(t, "hello_world returned: 10", stdout)
 	require.Equal(t, "sad sad world at assemblyscript.ts:7:3\n", stderr)
