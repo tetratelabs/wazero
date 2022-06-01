f4f6571c59508710d90061c4c6a27d3f5bba0c57
Adrian Cole <adrian@tetrate.io>
2022-06-01 12:33:52 +0800

    Narrows compiler support to operating systems currently known to work
    This narrows to what the `internal/platform` package supports, which is
    currently bound by Go SDK source that include `Mprotect` or windows.
    
    Ex. `zsyscall_linux_amd64.go` includes `Mprotect`, but
    `zsyscall_freebsd_amd64.go` does not.
    
    This should prevent errors like below, by allowing `wazero.NewRuntime()`
    to properly fallback to the interpreter.
    
    ```
    .../mmap.go:74:16: undefined: syscall.Mprotect
    ```
    
    A later change will implement FreeBSD. This is just here to ensure users
    don't crash on unexpected OS.
    
    See #607
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- config_supported.go
+++ config_supported.go
@@ -1,4 +1,14 @@
-//go:build amd64 || arm64
+// Note: The build constraints here are about the compiler, which is more
+// narrow than the architectures supported by the assembler.
+//
+// For example, the internal/platform package has dependencies such as
+// syscall.Mprotect when not on Windows. Go's SDK only supports this on darwin
+// and linux. Constraints here may loosen as the platform package supports
+// other runtime.GOOS, or a plugin becomes available to accomplish the same.
+//
+// Meanwhile, users who know their runtime.GOOS can operate with the compiler
+// may choose to use NewRuntimeConfigCompiler explicitly.
+//go:build (amd64 || arm64) && (darwin || linux || windows)
 
 package wazero
 
