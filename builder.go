15b3985b3c05213d8c757860188a3962b4ef90b9
Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>
2022-06-22 16:52:36 +0800

    Update builder.go

--- builder.go
+++ builder.go
@@ -173,6 +173,7 @@ type ModuleBuilder interface {
 	Compile(context.Context, CompileConfig) (CompiledModule, error)
 
 	// Instantiate is a convenience that calls Compile, then Namespace.InstantiateModule.
+	//
 	// Ex.
 	//
 	//	ctx := context.Background()
