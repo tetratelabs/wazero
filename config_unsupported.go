82f81242299c1dd19409fcb17d72c52013f3f37e
Adrian Cole <adrian@tetrate.io>
2022-07-01 15:11:50 +0800

    again
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- config_unsupported.go
+++ config_unsupported.go
@@ -1,5 +1,5 @@
 // This is the opposite constraint of config_supported.go
-//go:build !(amd64 || arm64) || !(darwin || linux || windows)
+//go:build !(amd64 || arm64) || !(darwin || linux || freebsd || windows)
 
 package wazero
 
