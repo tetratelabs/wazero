c11c460a7210943d132dd4a5227cf3839b9ee2ea
Takeshi Yoneda <takeshi@tetrate.io>
2022-08-12 11:22:59 +0800

    more fix
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- internal/asm/amd64/impl_2_test.go
+++ internal/asm/amd64/impl_2_test.go
@@ -606,6 +606,10 @@ func TestAssemblerImpl_EncodeRegisterToRegister(t *testing.T) {
 				n:      &nodeImpl{instruction: ADDL, types: operandTypesRegisterToRegister, srcReg: RegAX},
 				expErr: "invalid register [nil]",
 			},
+			{
+				n:      &nodeImpl{instruction: MOVL, types: operandTypesRegisterToRegister, srcReg: RegX0, dstReg: RegX1},
+				expErr: "MOVL for float to float is undefined",
+			},
 		}
 
 		for _, tc := range tests {
