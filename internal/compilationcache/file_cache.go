9210c4529dee7e17077ce0e3a58c352a4cd86e11
Takeshi Yoneda <takeshi@tetrate.io>
2022-08-18 15:25:45 +0900

    comment
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- internal/compilationcache/file_cache.go
+++ internal/compilationcache/file_cache.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"os"
 	"path"
+	"sync"
 )
 
 // FileCachePathKey is a context.Context Value key. It allows overriding fs.FS for WASI.
@@ -29,25 +30,45 @@ func newFileCache(dir string) *fileCache {
 // Note: this can be expanded to do binary signing/verification, set TTL on each entry, etc.
 type fileCache struct {
 	dirPath string
+	mux     sync.RWMutex
 }
 
-func (f *fileCache) path(key Key) string {
-	return path.Join(f.dirPath, hex.EncodeToString(key[:]))
+type fileReadCloser struct {
+	*os.File
+	fc *fileCache
 }
 
-func (f *fileCache) Get(key Key) (content io.ReadCloser, ok bool, err error) {
-	content, err = os.Open(f.path(key))
+func (fc *fileCache) path(key Key) string {
+	return path.Join(fc.dirPath, hex.EncodeToString(key[:]))
+}
+
+func (fc *fileCache) Get(key Key) (content io.ReadCloser, ok bool, err error) {
+	// TODO: take lock per key for more efficiency vs the complexity of impl.
+	// Unlock is done inside the content.Close().
+	fc.mux.RLock()
+	f, err := os.Open(fc.path(key))
 	if errors.Is(err, os.ErrNotExist) {
 		return nil, false, nil
 	} else if err != nil {
 		return nil, false, err
 	} else {
-		return content, true, nil
+		return &fileReadCloser{File: f, fc: fc}, true, nil
 	}
 }
 
-func (f *fileCache) Add(key Key, content io.Reader) (err error) {
-	file, err := os.Create(f.path(key))
+// Close wraps the os.File Close to release the read lock on fileCache.
+func (f *fileReadCloser) Close() (err error) {
+	defer f.fc.mux.RUnlock()
+	err = f.File.Close()
+	return
+}
+
+func (fc *fileCache) Add(key Key, content io.Reader) (err error) {
+	// TODO: take lock per key for more efficiency vs the complexity of impl.
+	fc.mux.Lock()
+	defer fc.mux.Unlock()
+
+	file, err := os.Create(fc.path(key))
 	if err != nil {
 		return
 	}
@@ -56,8 +77,12 @@ func (f *fileCache) Add(key Key, content io.Reader) (err error) {
 	return
 }
 
-func (f *fileCache) Delete(key Key) (err error) {
-	err = os.Remove(f.path(key))
+func (fc *fileCache) Delete(key Key) (err error) {
+	// TODO: take lock per key for more efficiency vs the complexity of impl.
+	fc.mux.Lock()
+	defer fc.mux.Unlock()
+
+	err = os.Remove(fc.path(key))
 	if errors.Is(err, os.ErrNotExist) {
 		err = nil
 	}
