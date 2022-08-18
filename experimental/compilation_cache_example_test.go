d32026879b6c1fd9a3c6e85753bd73066116eabf
Takeshi Yoneda <takeshi@tetrate.io>
2022-08-18 19:55:00 +0900

    Adds Example on compilation cache.
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

---
+++ experimental/compilation_cache_example_test.go
@@ -0,0 +1,47 @@
+package experimental_test
+
+import (
+	"context"
+	_ "embed"
+	"log"
+	"os"
+
+	"github.com/tetratelabs/wazero"
+	"github.com/tetratelabs/wazero/experimental"
+)
+
+// This is a basic example of using the file system compilation cache via WithCompilationCacheDirName.
+// The main goal is to show how it is configured.
+func Example_withCompilationCacheDirName() {
+	// Prepare a cache directory.
+	cacheDir := os.TempDir()
+	defer os.RemoveAll(cacheDir)
+
+	// Append the directory into the context for configuration.
+	ctx := experimental.WithCompilationCacheDirName(context.Background(), cacheDir)
+
+	// Create a first runtime and compile a binary.
+	r := wazero.NewRuntime(ctx)
+	defer r.Close(ctx) // This closes everything this Runtime created.
+
+	// Repeat newRuntimeCompileDestroy with the same cache directory.
+	newRuntimeCompileDestroy(ctx)
+	// After the second invocation, the actual compilation doesn't happen,
+	// and instead, the persisted file cache is re-used.
+	newRuntimeCompileDestroy(ctx)
+	newRuntimeCompileDestroy(ctx)
+
+	// Output:
+	//
+}
+
+// newRuntimeCompileDestroy creates a new wazero.Runtime, compile a binary, and then delete the runtime.
+func newRuntimeCompileDestroy(ctx context.Context) {
+	r := wazero.NewRuntime(ctx)
+	defer r.Close(ctx) // This closes everything this Runtime created except the file system cache.
+
+	_, err := r.CompileModule(ctx, fsWasm, wazero.NewCompileConfig())
+	if err != nil {
+		log.Panicln(err)
+	}
+}
