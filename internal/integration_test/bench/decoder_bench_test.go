d73e71ead3a683fb090163e11653f53db8ab1c45
Takeshi Yoneda <takeshi@tetrate.io>
2022-12-05 09:07:45 +0900

    decoder bench
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

---
+++ internal/integration_test/bench/decoder_bench_test.go
@@ -0,0 +1,20 @@
+package bench
+
+import (
+	"testing"
+
+	"github.com/tetratelabs/wazero/api"
+	"github.com/tetratelabs/wazero/internal/wasm"
+	"github.com/tetratelabs/wazero/internal/wasm/binary"
+)
+
+func BenchmarkCodec(b *testing.B) {
+	b.Run("binary.DecodeModule", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			if _, err := binary.DecodeModule(caseWasm, api.CoreFeaturesV2, wasm.MemoryLimitPages, false, false); err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+}
