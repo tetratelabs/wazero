9115b15898ee11c1e63e92df92ca5383bf031c02
Takeshi Yoneda <takeshi@tetrate.io>
2022-12-06 11:17:13 +0900

    more
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- internal/integration_test/fuzz/wazerolib/nodiff_test.go
+++ internal/integration_test/fuzz/wazerolib/nodiff_test.go
@@ -9,9 +9,9 @@ import (
 
 // TestReRunFailedRequireNoDiffCase re-runs the failed case specified by WASM_BINARY_NAME in testdata directory.
 func TestReRunFailedRequireNoDiffCase(t *testing.T) {
-	// binaryPath := os.Getenv("WASM_BINARY_PATH")
+	binaryPath := os.Getenv("WASM_BINARY_PATH")
 
-	wasmBin, err := os.ReadFile("/Users/mathetake/wazero/internal/integration_test/fuzz/wazerolib/testdata/750512a04967c7a63b2b7f16cca7d23dcea67341602f884bc8bff73abc68baf7.wasm")
+	wasmBin, err := os.ReadFile(binaryPath)
 	if err != nil {
 		t.Skip(err)
 	}
