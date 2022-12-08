bc7b79ea42c6e038d89fcab3feb5b21afe916021
Takeshi Yoneda <takeshi@tetrate.io>
2022-12-08 12:23:44 +0900

    preopen
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- imports/wasi_snapshot_preview1/example/testdata/zig/cat.zig
+++ imports/wasi_snapshot_preview1/example/testdata/zig/cat.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 const io = std.io;
 const os = std.os;
 const allocator = std.heap.page_allocator;
-const PreopenList = std.fs.wasi.PreopenList;
 const stdout = std.io.getStdOut();
 const warn = std.log.warn;
 
@@ -11,13 +10,7 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    // Pre-opened files are not available by default. When using WASI, you must
-    // manually initialize pre-opens to prevent openFile crashes.
-    //
-    // See https://ziglang.org/documentation/0.10.0/#WASI
-    var preopens = PreopenList.init(allocator);
-    defer preopens.deinit();
-    try std.os.initPreopensWasi(allocator, "/");
+    _ = try std.fs.wasi.preopensAlloc(allocator);
 
     // loop on the args, skipping the filename (args[0])
     for (args[1..args.len]) |arg| {
