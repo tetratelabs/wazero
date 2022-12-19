bb34084611a146f3e50fcc30bd031bbd147fea35
Takeshi Yoneda <takeshi@tetrate.io>
2022-12-19 17:43:50 +0900

    ok
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- internal/leb128/leb128_alloc_test.go
+++ internal/leb128/leb128_alloc_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/tetratelabs/wazero/internal/testing/require"
 )
 
-// TestLeb128NoAlloc ensures no allocation required in the leb128 package.
 func TestLeb128NoAlloc(t *testing.T) {
 	t.Run("LoadUint32", func(t *testing.T) {
 		result := testing.Benchmark(BenchmarkLoadUint32)
