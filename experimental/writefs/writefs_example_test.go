6000ce40c950a929b840b208b77f62d3c96ccf36
Adrian Cole <adrian@tetrate.io>
2022-12-31 11:13:03 +0800

    Removes internal dependency on fs.FS
    As noted in slack, we are unlikley to long term use fs.FS internally.
    This ensures we attempt to cast to syscallfs.FS for all I/O by panicing
    on fs.Open.
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- experimental/writefs/writefs_example_test.go
+++ experimental/writefs/writefs_example_test.go
@@ -2,6 +2,7 @@ package writefs_test
 
 import (
 	_ "embed"
+	"log"
 
 	"github.com/tetratelabs/wazero"
 	"github.com/tetratelabs/wazero/experimental/writefs"
@@ -12,5 +13,9 @@ var config wazero.ModuleConfig //nolint
 // This shows how to use writefs.DirFS to map paths relative to "/work/appA",
 // as "/". Unlike os.DirFS, these paths will be writable.
 func Example_dirFS() {
-	config = wazero.NewModuleConfig().WithFS(writefs.DirFS("/work/appA"))
+	fs, err := writefs.NewDirFS("/work/appA")
+	if err != nil {
+		log.Panicln(err)
+	}
+	config = wazero.NewModuleConfig().WithFS(fs)
 }
