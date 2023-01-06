3ee76023f88d61675db27d486d2068f8c5d1aca8
Achille Roussel <achille.roussel@gmail.com>
2023-01-06 17:44:35 +0000

    remove unused function in tests
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- internal/syscallfs/syscallfs_test.go
+++ internal/syscallfs/syscallfs_test.go
@@ -7,12 +7,9 @@ import (
 	"os"
 	"path"
 	pathutil "path"
-	"runtime"
 	"syscall"
 	"testing"
-	"time"
 
-	"github.com/tetratelabs/wazero/internal/platform"
 	"github.com/tetratelabs/wazero/internal/testing/require"
 )
 
@@ -105,81 +102,4 @@ func testFS_Open_Read(t *testing.T, tmpDir string, testFS FS) {
 		_, ok = f.(io.Writer)
 		require.False(t, ok)
 	})
-}
-
-func testFS_Utimes(t *testing.T, tmpDir string, testFS FS) {
-	file := "file"
-	err := os.WriteFile(path.Join(tmpDir, file), []byte{}, 0o700)
-	require.NoError(t, err)
-
-	dir := "dir"
-	err = os.Mkdir(path.Join(tmpDir, dir), 0o700)
-	require.NoError(t, err)
-
-	t.Run("doesn't exist", func(t *testing.T) {
-		err := testFS.Utimes("nope",
-			time.Unix(123, 4*1e3).UnixNano(),
-			time.Unix(567, 8*1e3).UnixNano())
-		require.Equal(t, syscall.ENOENT, err)
-	})
-
-	type test struct {
-		name                 string
-		path                 string
-		atimeNsec, mtimeNsec int64
-	}
-
-	// Note: This sets microsecond granularity because Windows doesn't support
-	// nanosecond.
-	tests := []test{
-		{
-			name:      "file positive",
-			path:      file,
-			atimeNsec: time.Unix(123, 4*1e3).UnixNano(),
-			mtimeNsec: time.Unix(567, 8*1e3).UnixNano(),
-		},
-		{
-			name:      "dir positive",
-			path:      dir,
-			atimeNsec: time.Unix(123, 4*1e3).UnixNano(),
-			mtimeNsec: time.Unix(567, 8*1e3).UnixNano(),
-		},
-		{name: "file zero", path: file},
-		{name: "dir zero", path: dir},
-	}
-
-	// linux and freebsd report inaccurate results when the input ts is negative.
-	if runtime.GOOS == "windows" || runtime.GOOS == "darwin" {
-		tests = append(tests,
-			test{
-				name:      "file negative",
-				path:      file,
-				atimeNsec: time.Unix(-123, -4*1e3).UnixNano(),
-				mtimeNsec: time.Unix(-567, -8*1e3).UnixNano(),
-			},
-			test{
-				name:      "dir negative",
-				path:      dir,
-				atimeNsec: time.Unix(-123, -4*1e3).UnixNano(),
-				mtimeNsec: time.Unix(-567, -8*1e3).UnixNano(),
-			},
-		)
-	}
-
-	for _, tt := range tests {
-		tc := tt
-		t.Run(tc.name, func(t *testing.T) {
-			err := testFS.Utimes(tc.path, tc.atimeNsec, tc.mtimeNsec)
-			require.NoError(t, err)
-
-			stat, err := os.Stat(path.Join(tmpDir, tc.path))
-			require.NoError(t, err)
-
-			atimeNsec, mtimeNsec, _ := platform.StatTimes(stat)
-			if platform.CompilerSupported() {
-				require.Equal(t, atimeNsec, tc.atimeNsec)
-			} // else only mtimes will return.
-			require.Equal(t, mtimeNsec, tc.mtimeNsec)
-		})
-	}
 }
