c179182e6ce602d5a1c1916aaddc277b7d7e26a7
Achille Roussel <achille.roussel@gmail.com>
2023-02-02 06:51:40 +0000

    make format
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- internal/platform/open_file_windows.go
+++ internal/platform/open_file_windows.go
@@ -15,14 +15,13 @@ import (
 // Since those placeholder are not interpreted by the open function, the unix
 // features they represent are also not implemented on windows:
 //
-// - O_DIRECTORY allows programs to ensure that the opened file is a directory.
-//   This could be emulated by doing a stat call on the file after opening it
-//   to verify that it is in fact a directory, then closing it and returning an
-//   error if it is not.
+//   - O_DIRECTORY allows programs to ensure that the opened file is a directory.
+//     This could be emulated by doing a stat call on the file after opening it
+//     to verify that it is in fact a directory, then closing it and returning an
+//     error if it is not.
 //
-// - O_NOFOLLOW allows programs to ensure that if the opened file is a symbolic
-//   link, the link itself is opened instead of its target.
-//
+//   - O_NOFOLLOW allows programs to ensure that if the opened file is a symbolic
+//     link, the link itself is opened instead of its target.
 const (
 	O_DIRECTORY = 1 << 29
 	O_NOFOLLOW  = 1 << 30
