7cfce0bfd7f569dd22e92e2b726a62c243cd52bd
Takeshi Yoneda <takeshi@tetrate.io>
2023-02-07 12:22:32 +0900

    fix js
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- internal/platform/open_file.go
+++ internal/platform/open_file.go
@@ -1,10 +1,18 @@
-//go:build !windows
+//go:build !windows && !js
 
 package platform
 
 import (
 	"io/fs"
 	"os"
+	"syscall"
+)
+
+// Simple aliases to constants in the syscall package for portability with
+// platforms which do not have them (e.g. windows)
+const (
+	O_DIRECTORY = syscall.O_DIRECTORY
+	O_NOFOLLOW  = syscall.O_NOFOLLOW
 )
 
 func OpenFile(name string, flag int, perm fs.FileMode) (*os.File, error) {
