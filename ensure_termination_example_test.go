5eb9216bec795eaf2f0af0785bf083afb9bddf30
Takeshi Yoneda <takeshi@tetrate.io>
2023-02-09 13:04:56 +0900

    more
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- ensure_termination_example_test.go
+++ ensure_termination_example_test.go
@@ -19,8 +19,6 @@ var infiniteLoopWasm []byte
 // of infinite loop function with context.Context created by context.WithTimeout powered by
 // RuntimeConfig.WithEnsureTermination configuration.
 func ExampleRuntimeConfig_WithEnsureTermination_context_timeout() {
-	ctx := context.Background()
-
 	r := wazero.NewRuntimeWithConfig(context.Background(),
 		// Enables the WithEnsureTermination option.
 		wazero.NewRuntimeConfig().WithEnsureTermination(true))
@@ -45,19 +43,17 @@ func ExampleRuntimeConfig_WithEnsureTermination_context_timeout() {
 	// Invoke the infinite loop with the timeout context.
 	_, err = infiniteLoop.Call(ctx)
 
-	// Timeout is correctly handled and causes the termination of infinite loop.
+	// Timeout is correctly handled and triggers the termination of infinite loop.
 	fmt.Println(err)
 
 	// Output:
 	//	module "malicious_wasm" closed with context deadline exceeded
 }
 
-// ExampleRuntimeConfig_WithEnsureTermination_context_timeout demonstrates how to ensure the termination
+// ExampleRuntimeConfig_WithEnsureTermination_context_cancel demonstrates how to ensure the termination
 // of infinite loop function with context.Context created by context.WithCancel powered by
 // RuntimeConfig.WithEnsureTermination configuration.
 func ExampleRuntimeConfig_WithEnsureTermination_context_cancel() {
-	ctx := context.Background()
-
 	r := wazero.NewRuntimeWithConfig(context.Background(),
 		// Enables the WithEnsureTermination option.
 		wazero.NewRuntimeConfig().WithEnsureTermination(true))
@@ -86,9 +82,49 @@ func ExampleRuntimeConfig_WithEnsureTermination_context_cancel() {
 	// Invoke the infinite loop with the timeout context.
 	_, err = infiniteLoop.Call(ctx)
 
-	// context Cancelation is correctly handled and causes the termination of infinite loop.
+	// context Cancelation is correctly handled and triggers the termination of infinite loop.
 	fmt.Println(err)
 
 	// Output:
 	//	module "malicious_wasm" closed with context canceled
+}
+
+// ExampleRuntimeConfig_WithEnsureTermination_moduleClose demonstrates how to ensure the termination
+// of infinite loop function with api.Module's CloseWithExitCode method powered by
+// RuntimeConfig.WithEnsureTermination configuration.
+func ExampleRuntimeConfig_WithEnsureTermination_moduleClose() {
+	r := wazero.NewRuntimeWithConfig(context.Background(),
+		// Enables the WithEnsureTermination option.
+		wazero.NewRuntimeConfig().WithEnsureTermination(true))
+
+	compiledModule, err := r.CompileModule(context.Background(), infiniteLoopWasm)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	moduleInstance, err := r.InstantiateModule(context.Background(), compiledModule,
+		wazero.NewModuleConfig().WithName("malicious_wasm"))
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	infiniteLoop := moduleInstance.ExportedFunction("infinite_loop")
+
+	go func() {
+		// After 2 seconds, close the module instance with CloseWithExitCode, which triggers the termination
+		// of infinite loop.
+		time.Sleep(2 * time.Second)
+		if err := moduleInstance.CloseWithExitCode(context.Background(), 1); err != nil {
+			log.Panicln(err)
+		}
+	}()
+
+	// Invoke the infinite loop with the timeout context.
+	_, err = infiniteLoop.Call(context.Background())
+
+	// The exit code is correctly handled and triggers the termination of infinite loop.
+	fmt.Println(err)
+
+	// Output:
+	//	module "malicious_wasm" closed with exit_code(1)
 }
