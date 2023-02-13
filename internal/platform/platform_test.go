fdd9a7cccddc7170027004d8d9440e2899da0339
Takeshi Yoneda <takeshi@tetrate.io>
2023-02-14 08:56:09 +0900

    fuzz
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- internal/platform/platform_test.go
+++ internal/platform/platform_test.go
@@ -11,7 +11,7 @@ func TestCompiler_archRequirementsVerified(t *testing.T) {
 	case "arm64":
 		require.True(t, archRequirementsVerified)
 	case "amd64":
-		// TODO: once we find a way to test no SSE4 platform, you build tag and choose the correct assertion.
+		// TODO: once we find a way to test no SSE4 platform, use build tag and choose the correct assertion.
 		// For now, we assume that all the amd64 machine we are testing are with SSE 4 to avoid
 		// accidentally turn off compiler on the modern amd64 platform.
 		require.True(t, archRequirementsVerified)
