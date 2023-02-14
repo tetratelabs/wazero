eb024c6d0d36896f18f7c6fa57d71ed5eb3537e8
Takeshi Yoneda <takeshi@tetrate.io>
2023-02-14 15:21:55 +0900

    fmt
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- internal/platform/time_test.go
+++ internal/platform/time_test.go
@@ -1,11 +1,11 @@
 package platform
 
 import (
-	"github.com/tetratelabs/wazero/sys"
 	"testing"
 	"time"
 
 	"github.com/tetratelabs/wazero/internal/testing/require"
+	"github.com/tetratelabs/wazero/sys"
 )
 
 func Test_NewFakeWalltime(t *testing.T) {
