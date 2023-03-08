89f918105a04536f39fcb2f08f9f237aac10f68e
Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>
2023-03-08 09:47:10 +0800

    platform: adds notes about darwin and CGO (#1212)
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- internal/platform/bench_test.go
+++ internal/platform/bench_test.go
@@ -2,11 +2,28 @@ package platform
 
 import (
 	"os"
+	"path"
 	"testing"
 )
 
 func Benchmark_IsTerminal(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		IsTerminal(os.Stdout.Fd())
+	}
+}
+
+func Benchmark_UtimesNano(b *testing.B) {
+	tmpDir := b.TempDir()
+	f, err := os.Create(path.Join(tmpDir, "file"))
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer f.Close()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := UtimesNanoFile(f, 1, 1); err != nil {
+			b.Fatal(err)
+		}
 	}
 }
