d35b17f1798c355fa7be5c6f997c254f90510fcc
Takeshi Yoneda <takeshi@tetrate.io>
2023-03-08 13:05:32 +0900

    more fix
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- internal/integration_test/engine/adhoc_test.go
+++ internal/integration_test/engine/adhoc_test.go
@@ -500,7 +500,7 @@ func callHostFunctionIndirect(t *testing.T, r wazero.Runtime) {
 		ImportSection:   []*wasm.Import{{Module: hostModule, Name: hostFn, Type: wasm.ExternTypeFunc, DescFunc: 0}},
 		FunctionSection: []wasm.Index{0},
 		ExportSection:   []*wasm.Export{{Name: importingWasmModuleFn, Type: wasm.ExternTypeFunc, Index: 1}},
-		CodeSection:     []*wasm.Code{{Body: []byte{wasm.OpcodeCall, 0, wasm.OpcodeUnreachable, wasm.OpcodeEnd}}},
+		CodeSection:     []*wasm.Code{{Body: []byte{wasm.OpcodeCall, 0, wasm.OpcodeEnd}}},
 		NameSection:     &wasm.NameSection{ModuleName: importingWasmModule},
 	}
 
