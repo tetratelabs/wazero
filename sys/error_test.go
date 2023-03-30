b1dd5d900075e5f313dfc768eb05cffb347e35a8
Alan Braithwaite <alan@braithwaite.dev>
2023-03-29 20:21:19 -0700

    sys/error: handle errors.Is for common context use-cases (#1313)
    Signed-off-by: Alan Braithwaite <alan@runreveal.com>

--- sys/error_test.go
+++ sys/error_test.go
@@ -1,6 +1,7 @@
 package sys
 
 import (
+	"context"
 	"errors"
 	"testing"
 
@@ -55,11 +56,13 @@ func TestExitError_Error(t *testing.T) {
 		err := NewExitError(ExitCodeDeadlineExceeded)
 		require.Equal(t, ExitCodeDeadlineExceeded, err.ExitCode())
 		require.EqualError(t, err, "module closed with context deadline exceeded")
+		require.ErrorIs(t, err, context.DeadlineExceeded, "exit code context deadline exceeded should work")
 	})
 	t.Run("cancel", func(t *testing.T) {
 		err := NewExitError(ExitCodeContextCanceled)
 		require.Equal(t, ExitCodeContextCanceled, err.ExitCode())
 		require.EqualError(t, err, "module closed with context canceled")
+		require.ErrorIs(t, err, context.Canceled, "exit code context canceled should work")
 	})
 	t.Run("normal", func(t *testing.T) {
 		err := NewExitError(123)
