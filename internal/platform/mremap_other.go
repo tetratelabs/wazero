cb06b7b77953c6a8f99b99e42d84ed6a4aa8a0f4
Edoardo Vacchi <evacchi@users.noreply.github.com>
2023-05-16 06:06:29 +0200

    platform: fix minor linting issue (#1471)
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>
    Signed-off-by: Adrian Cole <adrian@tetrate.io>
    Co-authored-by: Adrian Cole <adrian@tetrate.io>

--- internal/platform/mremap_other.go
+++ internal/platform/mremap_other.go
@@ -8,8 +8,23 @@ func remapCodeSegmentAMD64(code []byte, size int) ([]byte, error) {
 		return nil, err
 	}
 	copy(b, code)
-	munmapCodeSegment(code)
+	mustMunmapCodeSegment(code)
 	return b, nil
+}
+
+// mustMunmapCodeSegment panics instead of returning an error to the
+// application.
+//
+// # Why panic?
+//
+// It is less disruptive to the application to leak the previous block if it
+// could be unmapped than to leak the new block and return an error.
+// Realistically, either scenarios are pretty hard to debug, so we panic. There
+// is less as the dominant runtime.GOOS (linux) doesn't use this anyway.
+func mustMunmapCodeSegment(code []byte) {
+	if err := munmapCodeSegment(code); err != nil {
+		panic(err)
+	}
 }
 
 func remapCodeSegmentARM64(code []byte, size int) ([]byte, error) {
@@ -18,6 +33,6 @@ func remapCodeSegmentARM64(code []byte, size int) ([]byte, error) {
 		return nil, err
 	}
 	copy(b, code)
-	munmapCodeSegment(code)
+	mustMunmapCodeSegment(code)
 	return b, nil
 }
