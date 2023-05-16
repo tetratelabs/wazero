4341c5ee9b32cd7ea706c61791c8e73f37c21315
Adrian Cole <adrian@tetrate.io>
2023-05-16 11:26:50 +0800

    must panic
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- internal/platform/mremap_other.go
+++ internal/platform/mremap_other.go
@@ -8,11 +8,23 @@ func remapCodeSegmentAMD64(code []byte, size int) ([]byte, error) {
 		return nil, err
 	}
 	copy(b, code)
-	err = munmapCodeSegment(code)
-	if err != nil {
-		return nil, err
-	}
+	mustMunmapCodeSegment(code)
 	return b, nil
+}
+
+// mustMunmapCodeSegment panics instead of returning an error to the
+// application.
+//
+// # Why panic?
+//
+// It is less disruptive to the application to leak the previous block if it
+// could be unmapped than to leak the new block and return an error.
+// Realistically, either scenarios are pretty hard to debug, so we panic. There
+// is less as the dominant runtime.GOOS (linux) doesn't use this anyway.
+func mustMunmapCodeSegment(code []byte) {
+	if err := munmapCodeSegment(code); err != nil {
+		panic(err)
+	}
 }
 
 func remapCodeSegmentARM64(code []byte, size int) ([]byte, error) {
@@ -21,9 +33,6 @@ func remapCodeSegmentARM64(code []byte, size int) ([]byte, error) {
 		return nil, err
 	}
 	copy(b, code)
-	err = munmapCodeSegment(code)
-	if err != nil {
-		return nil, err
-	}
+	mustMunmapCodeSegment(code)
 	return b, nil
 }
