95d450a7d00f5e3648507abf36d281ed4bdf9c92
Anuraag Agrawal <anuraaga@gmail.com>
2023-05-16 12:31:02 +0900

    Document atomics are guest-only
    Signed-off-by: Anuraag Agrawal <anuraaga@gmail.com>

--- experimental/features.go
+++ experimental/features.go
@@ -10,4 +10,5 @@ import "github.com/tetratelabs/wazero/api"
 //     wazero.NewRuntimeConfigInterpreter
 //   - The instruction list is too long to enumerate in godoc.
 //     See https://github.com/WebAssembly/threads/blob/main/proposals/threads/Overview.md
+//   - Atomic operations are guest-only until api.Memory or otherwise expose them to host functions.
 const CoreFeaturesThreads = api.CoreFeatureSIMD << 1 // TODO: Implement the compiler engine
