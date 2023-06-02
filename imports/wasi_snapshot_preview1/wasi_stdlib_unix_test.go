9f4ee38c7082e1c6c79c4a6652e91f05f16f5c9f
Edoardo Vacchi <evacchi@users.noreply.github.com>
2023-06-02 16:17:48 +0200

    wasi: non-blocking i/o on files
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

---
+++ imports/wasi_snapshot_preview1/wasi_stdlib_unix_test.go
@@ -0,0 +1,47 @@
+//go:build unix || linux || darwin
+
+package wasi_snapshot_preview1_test
+
+import (
+	"os"
+	"strings"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/tetratelabs/wazero"
+	"github.com/tetratelabs/wazero/internal/testing/require"
+)
+
+func Test_Nonblock(t *testing.T) {
+	const fifo = "/test-fifo"
+	tempDir := t.TempDir()
+	fifoAbsPath := tempDir + fifo
+
+	moduleConfig := wazero.NewModuleConfig().
+		WithArgs("wasi", "nonblock", fifo).
+		WithFSConfig(wazero.NewFSConfig().WithDirMount(tempDir, "/")).
+		WithSysNanosleep()
+
+	err := syscall.Mkfifo(fifoAbsPath, 0o666)
+	require.NoError(t, err)
+
+	ch := make(chan string, 1)
+	go func() { ch <- compileAndRun(t, testCtx, moduleConfig, wasmZigCc) }()
+
+	// The test writes a few dots on the console until the pipe has data ready for reading,
+	// so we wait for a little to ensure those dots are printed.
+	time.Sleep(500 * time.Millisecond)
+
+	f, err := os.OpenFile(fifoAbsPath, os.O_APPEND|os.O_WRONLY, 0)
+	require.NoError(t, err)
+	n, err := f.Write([]byte("wazero"))
+	require.NoError(t, err)
+	require.NotEqual(t, 0, n)
+	console := <-ch
+	lines := strings.Split(console, "\n")
+
+	// Check if the first line starts with at least one dot.
+	require.True(t, strings.HasPrefix(lines[0], "."))
+	require.Equal(t, "wazero", lines[1])
+}
