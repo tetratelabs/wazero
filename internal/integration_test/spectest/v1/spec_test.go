ed4651dffb856fa1ff2fe8e4f54eb8ec3231a109
Takeshi Yoneda <t.y.mathetake@gmail.com>
2023-10-12 09:23:06 +0900

    wazevo: ensures passing all v1/v2 spectests
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- internal/integration_test/spectest/v1/spec_test.go
+++ internal/integration_test/spectest/v1/spec_test.go
@@ -2,10 +2,12 @@ package v1
 
 import (
 	"context"
+	"runtime"
 	"testing"
 
 	"github.com/tetratelabs/wazero"
 	"github.com/tetratelabs/wazero/api"
+	"github.com/tetratelabs/wazero/internal/engine/wazevo"
 	"github.com/tetratelabs/wazero/internal/integration_test/spectest"
 	"github.com/tetratelabs/wazero/internal/platform"
 )
@@ -19,4 +21,13 @@ func TestCompiler(t *testing.T) {
 
 func TestInterpreter(t *testing.T) {
 	spectest.Run(t, Testcases, context.Background(), wazero.NewRuntimeConfigInterpreter().WithCoreFeatures(api.CoreFeaturesV1))
+}
+
+func TestWazevo(t *testing.T) {
+	c := wazero.NewRuntimeConfigCompiler().WithCoreFeatures(api.CoreFeaturesV1)
+	if runtime.GOARCH != "arm64" {
+		t.Skip()
+	}
+	wazevo.ConfigureWazevo(c)
+	spectest.Run(t, Testcases, context.Background(), c)
 }
