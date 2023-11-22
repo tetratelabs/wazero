17427e60e9d39839c1bac543233c9a0726bf5d31
Takeshi Yoneda <t.y.mathetake@gmail.com>
2023-11-23 08:29:10 +0900

    fuzz: adds support for minimization via `wasm-tools shrink` (#1844)
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

---
+++ internal/integration_test/fuzz/predicate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# The Wasm file is given as the first and only argument to the script.
+WASM=$1
+
+echo "Testing $WASM"
+
+export WASM_BINARY_PATH=$WASM
+
+# Run the test and reverse the exit code so that a non-zero exit code indicates interesting case.
+./nodiff.test
+exit $((! $?))
