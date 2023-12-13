c773341b7245b558ccdd500f6b6bac9007495ecc
Takeshi Yoneda <t.y.mathetake@gmail.com>
2023-12-13 09:54:20 -0800

    in CLI
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- internal/integration_test/fuzz/wazerolib/lib.go
+++ internal/integration_test/fuzz/wazerolib/lib.go
@@ -7,7 +7,6 @@ import (
 	_ "embed"
 	"encoding/hex"
 	"fmt"
-	"github.com/tetratelabs/wazero/experimental/opt"
 	"math"
 	"os"
 	"path"
@@ -15,6 +14,7 @@ import (
 	"strings"
 
 	"github.com/tetratelabs/wazero"
+	"github.com/tetratelabs/wazero/experimental/opt"
 	"github.com/tetratelabs/wazero/internal/leb128"
 	"github.com/tetratelabs/wazero/internal/testing/binaryencoding"
 	"github.com/tetratelabs/wazero/internal/wasm"
