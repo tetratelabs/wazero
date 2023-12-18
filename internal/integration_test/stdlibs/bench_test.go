1c2a9aba022a5d97bd9ac3f8d849bacebfb1349b
Edoardo Vacchi <evacchi@users.noreply.github.com>
2023-12-18 22:38:36 +0100

    use table-based tests
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- internal/integration_test/stdlibs/bench_test.go
+++ internal/integration_test/stdlibs/bench_test.go
@@ -30,155 +30,111 @@ var configs = []struct {
 }
 
 func BenchmarkStdlibs(b *testing.B) {
-	b.Run("zig", benchmarkZig)
-	b.Run("tinygo", benchmarkTinyGo)
-	b.Run("gowasip1", benchmarkGoWasip1)
-}
-
-func benchmarkZig(b *testing.B) {
-	dir := "testdata/zig/"
-	ctx := context.Background()
-
-	modCfg := defaultModuleConfig().
-		WithFSConfig(wazero.NewFSConfig().WithDirMount(".", "/")).
-		WithArgs("test.wasm")
-
-	files, err := os.ReadDir(dir)
-	require.NoError(b, err)
-	for _, f := range files {
-		bin, err := os.ReadFile(dir + f.Name())
-		require.NoError(b, err)
-		b.Run(f.Name(), func(b *testing.B) {
-			for _, cfg := range configs {
-				r := wazero.NewRuntimeWithConfig(ctx, cfg.config)
-				wasi_snapshot_preview1.MustInstantiate(ctx, r)
-				b.Cleanup(func() { r.Close(ctx) })
-
-				m, err := r.CompileModule(ctx, bin)
-				require.NoError(b, err)
-
-				b.Run(cfg.name, func(b *testing.B) {
-					b.Run("Compile", func(b *testing.B) {
-						_, err := r.CompileModule(ctx, bin)
-						require.NoError(b, err)
-					})
-					im, err := r.InstantiateModule(ctx, m, modCfg)
-					require.NoError(b, err)
-					b.Run("Run", func(b *testing.B) {
-						_, err := im.ExportedFunction("_start").Call(ctx)
-						requireZeroExitCode(b, err)
-					})
-				})
-			}
-		})
-	}
-}
-
-func benchmarkTinyGo(b *testing.B) {
-	dir := "testdata/tinygo/"
-	ctx := context.Background()
-
-	files, err := os.ReadDir(dir)
-	require.NoError(b, err)
-	for _, f := range files {
-		if !strings.HasSuffix(f.Name(), ".test") {
-			continue
-		}
-		bin, err := os.ReadFile(dir + f.Name())
-		fsconfig := wazero.NewFSConfig().
-			WithDirMount(".", "/").
-			WithDirMount("/tmp", "/tmp")
-		modCfg := defaultModuleConfig().
-			WithFSConfig(fsconfig).
-			WithArgs(f.Name(), "-test.v")
-
-		require.NoError(b, err)
-		b.Run(f.Name(), func(b *testing.B) {
-			for _, cfg := range configs {
-				r := wazero.NewRuntimeWithConfig(ctx, cfg.config)
-				wasi_snapshot_preview1.MustInstantiate(ctx, r)
-				b.Cleanup(func() { r.Close(ctx) })
-
-				m, err := r.CompileModule(ctx, bin)
-				require.NoError(b, err)
-
-				b.Run(cfg.name, func(b *testing.B) {
-					b.Run("Compile", func(b *testing.B) {
-						_, err := r.CompileModule(ctx, bin)
-						require.NoError(b, err)
-					})
-					im, err := r.InstantiateModule(ctx, m, modCfg)
-					require.NoError(b, err)
-					b.Run("Run", func(b *testing.B) {
-						_, err := im.ExportedFunction("_start").Call(ctx)
-						requireZeroExitCode(b, err)
-					})
-				})
-			}
-		})
-	}
-}
-
-func benchmarkGoWasip1(b *testing.B) {
 	cwd, _ := os.Getwd()
 	defer os.Chdir(cwd)
-	dir := "testdata/go/"
 	ctx := context.Background()
 
-	files, err := os.ReadDir(dir)
-	require.NoError(b, err)
-	for _, f := range files {
-		if !strings.HasSuffix(f.Name(), ".test") {
-			continue
-		}
-		os.Chdir(cwd)
-		fpath := filepath.Join(cwd, dir, f.Name())
-		bin, err := os.ReadFile(fpath)
-		fsuffixstripped := strings.ReplaceAll(f.Name(), ".test", "")
-		inferredpath := strings.ReplaceAll(fsuffixstripped, "_", "/")
-		testdir := filepath.Join(runtime.GOROOT(), inferredpath)
+	testCases := []struct {
+		name, dir    string
+		readTestCase func(fpath string, fname string) ([]byte, wazero.ModuleConfig, error)
+	}{
+		{
+			name: "zig",
+			dir:  "testdata/zig/",
+			readTestCase: func(fpath string, fname string) ([]byte, wazero.ModuleConfig, error) {
+				bin, err := os.ReadFile(fpath)
+				modCfg := defaultModuleConfig().
+					WithFSConfig(wazero.NewFSConfig().WithDirMount(".", "/")).
+					WithArgs("test.wasm")
 
-		os.Chdir(testdir)
-		modCfg := defaultModuleConfig().
-			WithFSConfig(
-				wazero.NewFSConfig().
-					WithDirMount("/", "/")).
-			WithEnv("PWD", testdir).
-			WithArgs(f.Name(), "-test.short", "-test.v")
+				return bin, modCfg, err
+			},
+		},
+		{
+			name: "tinygo",
+			dir:  "testdata/tinygo/",
+			readTestCase: func(fpath string, fname string) ([]byte, wazero.ModuleConfig, error) {
+				if !strings.HasSuffix(fname, ".test") {
+					return nil, nil, nil
+				}
+				bin, err := os.ReadFile(fpath)
+				fsconfig := wazero.NewFSConfig().
+					WithDirMount(".", "/").
+					WithDirMount("/tmp", "/tmp")
+				modCfg := defaultModuleConfig().
+					WithFSConfig(fsconfig).
+					WithArgs(fname, "-test.v")
 
-		require.NoError(b, err)
-		b.Run(f.Name(), func(b *testing.B) {
-			for _, cfg := range configs {
-				r := wazero.NewRuntimeWithConfig(ctx, cfg.config)
-				wasi_snapshot_preview1.MustInstantiate(ctx, r)
-				b.Cleanup(func() { r.Close(ctx) })
+				return bin, modCfg, err
+			},
+		},
+		{
+			name: "wasip1",
+			dir:  "testdata/go/",
+			readTestCase: func(fpath string, fname string) ([]byte, wazero.ModuleConfig, error) {
+				if !strings.HasSuffix(fname, ".test") {
+					return nil, nil, nil
+				}
 
-				m, err := r.CompileModule(ctx, bin)
+				bin, err := os.ReadFile(fpath)
+				fsuffixstripped := strings.ReplaceAll(fname, ".test", "")
+				inferredpath := strings.ReplaceAll(fsuffixstripped, "_", "/")
+				testdir := filepath.Join(runtime.GOROOT(), inferredpath)
+
+				os.Chdir(testdir)
+				modCfg := defaultModuleConfig().
+					WithFSConfig(
+						wazero.NewFSConfig().
+							WithDirMount("/", "/")).
+					WithEnv("PWD", testdir).
+					WithArgs(fname, "-test.short", "-test.v")
+				return bin, modCfg, err
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		b.Run(tc.name, func(b *testing.B) {
+			files, err := os.ReadDir(tc.dir)
+			require.NoError(b, err)
+			for _, f := range files {
+				fname := f.Name()
+				// Ensure we are on root dir.
+				os.Chdir(cwd)
+
+				fpath := filepath.Join(cwd, tc.dir, fname)
+				bin, modCfg, err := tc.readTestCase(fpath, fname)
 				require.NoError(b, err)
+				if bin == nil {
+					// skip
+					continue
+				}
 
-				b.Run(cfg.name, func(b *testing.B) {
-					b.Run("Compile", func(b *testing.B) {
-						_, err := r.CompileModule(ctx, bin)
+				b.Run(fname, func(b *testing.B) {
+					for _, cfg := range configs {
+						r := wazero.NewRuntimeWithConfig(ctx, cfg.config)
+						wasi_snapshot_preview1.MustInstantiate(ctx, r)
+						b.Cleanup(func() { r.Close(ctx) })
+
+						m, err := r.CompileModule(ctx, bin)
 						require.NoError(b, err)
-					})
-					im, err := r.InstantiateModule(ctx, m, modCfg)
-					require.NoError(b, err)
-					b.Run("Run", func(b *testing.B) {
-						_, err := im.ExportedFunction("_start").Call(ctx)
-						requireZeroExitCode(b, err)
-					})
+
+						b.Run(cfg.name, func(b *testing.B) {
+							b.Run("Compile", func(b *testing.B) {
+								_, err := r.CompileModule(ctx, bin)
+								require.NoError(b, err)
+							})
+							im, err := r.InstantiateModule(ctx, m, modCfg)
+							require.NoError(b, err)
+							b.Run("Run", func(b *testing.B) {
+								_, err := im.ExportedFunction("_start").Call(ctx)
+								requireZeroExitCode(b, err)
+							})
+						})
+					}
 				})
 			}
 		})
-	}
-}
-
-func requireZeroExitCode(b *testing.B, err error) {
-	b.Helper()
-	if se, ok := err.(*sys.ExitError); ok {
-		if se.ExitCode() != 0 { // Don't err on success.
-			require.NoError(b, err)
-		}
 	}
 }
 
@@ -192,4 +148,13 @@ func defaultModuleConfig() wazero.ModuleConfig {
 		WithStdout(os.Stdout).
 		WithStderr(os.Stderr).
 		WithStartFunctions()
+}
+
+func requireZeroExitCode(b *testing.B, err error) {
+	b.Helper()
+	if se, ok := err.(*sys.ExitError); ok {
+		if se.ExitCode() != 0 { // Don't err on success.
+			require.NoError(b, err)
+		}
+	}
 }
