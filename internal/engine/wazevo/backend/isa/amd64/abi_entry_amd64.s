d882eaaff11812722aa389705f761d487a723d3f
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-01-11 14:30:47 -0800

    fmt
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- internal/engine/wazevo/backend/isa/amd64/abi_entry_amd64.s
+++ internal/engine/wazevo/backend/isa/amd64/abi_entry_amd64.s
@@ -3,12 +3,12 @@
 
 // entrypoint(preambleExecutable, functionExecutable *byte, executionContextPtr uintptr, moduleContextPtr *byte, paramResultPtr *uint64, goAllocatedStackSlicePtr uintptr
 TEXT ·entrypoint(SB), NOSPLIT|NOFRAME, $0-48
-	MOVD preambleExecutable+0(FP), R11
+	MOVQ preambleExecutable+0(FP), R11
 	MOVQ functionExectuable+8(FP), R14
-	MOVQ executionContextPtr+16(FP), AX // First argument is passed in AX.
-	MOVD moduleContextPtr+24(FP), CX // Second argument is passed in CX.
-	MOVD paramResultSlicePtr+32(FP), R12
-	MOVD goAllocatedStackSlicePtr+40(FP), BX
+	MOVQ executionContextPtr+16(FP), AX      // First argument is passed in AX.
+	MOVQ moduleContextPtr+24(FP), CX         // Second argument is passed in CX.
+	MOVQ paramResultSlicePtr+32(FP), R12
+	MOVQ goAllocatedStackSlicePtr+40(FP), BX
 	JMP  R11
 
 TEXT ·afterGoFunctionCallEntrypoint(SB), NOSPLIT|NOFRAME, $0-24
