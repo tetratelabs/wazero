18bb5e6c8ac97e00fc55e10d34687420e9d74177
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-02-09 12:29:36 -0800

    fuzz
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- internal/integration_test/fuzz/wazerolib/extern.go
+++ internal/integration_test/fuzz/wazerolib/extern.go
@@ -2,8 +2,17 @@ package main
 
 import "C"
 import (
+	"context"
+	"math"
 	"reflect"
+	"strings"
 	"unsafe"
+
+	"github.com/tetratelabs/wazero"
+	"github.com/tetratelabs/wazero/experimental/opt"
+	"github.com/tetratelabs/wazero/internal/leb128"
+	"github.com/tetratelabs/wazero/internal/testing/binaryencoding"
+	"github.com/tetratelabs/wazero/internal/wasm"
 )
 
 func main() {}
@@ -59,4 +68,73 @@ func validate(binaryPtr uintptr, binarySize int) {
 
 	tryCompile(wasmBin)
 	failed = false
+}
+
+//export test_signal_stack
+func test_signal_stack() {
+	// (module
+	//  (func (export "long_loop")
+	//    (loop
+	//      global.get 0
+	//      i32.eqz
+	//      if  ;; label = @1
+	//        unreachable
+	//      end
+	//      global.get 0
+	//      i32.const 1
+	//      i32.sub
+	//      global.set 0
+	//      br 0
+	//    )
+	//  )
+	//  (global (;0;) (mut i32) i32.const 2147483648)
+	// )
+	bin := binaryencoding.EncodeModule(&wasm.Module{
+		TypeSection:     []wasm.FunctionType{{}},
+		FunctionSection: []wasm.Index{0},
+		GlobalSection: []wasm.Global{{
+			Type: wasm.GlobalType{ValType: wasm.ValueTypeI32, Mutable: true},
+			Init: wasm.ConstantExpression{
+				Opcode: wasm.OpcodeI32Const,
+				Data:   leb128.EncodeInt32(math.MaxInt32),
+			},
+		}},
+		ExportSection: []wasm.Export{{Type: wasm.ExternTypeFunc, Name: "long_loop", Index: 0}},
+		CodeSection: []wasm.Code{
+			{
+				Body: []byte{
+					wasm.OpcodeLoop, 0,
+					wasm.OpcodeGlobalGet, 0,
+					wasm.OpcodeI32Eqz,
+					wasm.OpcodeIf, 0,
+					wasm.OpcodeUnreachable,
+					wasm.OpcodeEnd,
+					wasm.OpcodeGlobalGet, 0,
+					wasm.OpcodeI32Const, 1,
+					wasm.OpcodeI32Sub,
+					wasm.OpcodeGlobalSet, 0,
+					wasm.OpcodeBr, 0,
+					wasm.OpcodeEnd,
+					wasm.OpcodeEnd,
+				},
+			},
+		},
+	})
+	ctx := context.Background()
+	config := opt.NewRuntimeConfigOptimizingCompiler()
+	r := wazero.NewRuntimeWithConfig(ctx, config)
+	module, err := r.Instantiate(ctx, bin)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = module.Close(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	_, err = module.ExportedFunction("long_loop").Call(ctx)
+	if !strings.Contains(err.Error(), "unreachable") {
+		panic("long_loop should be unreachable")
+	}
 }
