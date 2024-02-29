28cc3f22ec890998c66776525d616037279e6af0
Edoardo Vacchi <evacchi@users.noreply.github.com>
2024-02-29 17:51:08 +0100

    Revert "spectest: compile+instantiate module on `assert_uninstantiable` to avoid auto-close"
    This reverts commit 5ddc1508c3ba66a89ab04f68b7d2199a9e97baaf.

--- internal/integration_test/spectest/spectest.go
+++ internal/integration_test/spectest/spectest.go
@@ -498,12 +498,7 @@ func RunCase(t *testing.T, testDataFS embed.FS, f string, ctx context.Context, c
 				case "assert_uninstantiable":
 					buf, err := testDataFS.ReadFile(testdataPath(c.Filename))
 					require.NoError(t, err, msg)
-					// linking.wast references this module later, so while the instantiation
-					// should fail, we should not close it. InstantiateWithConfig() will auto-close
-					// on failure, so instead we compile then instantiate explicitly.
-					cm, err := r.CompileModule(ctx, buf)
-					require.NoError(t, err, msg)
-					_, err = r.InstantiateModule(ctx, cm, wazero.NewModuleConfig())
+					_, err = r.InstantiateWithConfig(ctx, buf, wazero.NewModuleConfig())
 					if c.Text == "out of bounds table access" {
 						// This is not actually an instantiation error, but assert_trap in the original wast, but wast2json translates it to assert_uninstantiable.
 						// Anyway, this spectest case expects the error due to active element offset ouf of bounds
