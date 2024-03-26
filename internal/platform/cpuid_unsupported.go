35eb4242994fd14c7b8f821501c14a551da17938
gram <git@orsinium.dev>
2024-03-26 10:05:12 +0100

    drop 'unused' cpuid func
    Signed-off-by: gram <git@orsinium.dev>

--- internal/platform/cpuid_unsupported.go
+++ internal/platform/cpuid_unsupported.go
@@ -12,7 +12,3 @@ func (c *cpuFeatureFlags) Has(cpuFeature CpuFeature) bool { return false }
 
 // HasExtra implements the same method on the CpuFeatureFlags interface
 func (c *cpuFeatureFlags) HasExtra(cpuFeature CpuFeature) bool { return false }
-
-func cpuid(arg1, arg2 uint32) (eax, ebx, ecx, edx uint32) {
-	panic("unsupported")
-}
