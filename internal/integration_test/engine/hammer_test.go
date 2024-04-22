621f62d5a49fdb3ebee1313d1098a6f060aab210
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-04-22 16:10:04 +0900

    Fixes race condition in concurrent engine usage (#2194)
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- internal/integration_test/engine/hammer_test.go
+++ internal/integration_test/engine/hammer_test.go
@@ -8,15 +8,18 @@ import (
 	"github.com/tetratelabs/wazero"
 	"github.com/tetratelabs/wazero/api"
 	"github.com/tetratelabs/wazero/internal/platform"
+	"github.com/tetratelabs/wazero/internal/testing/binaryencoding"
 	"github.com/tetratelabs/wazero/internal/testing/hammer"
 	"github.com/tetratelabs/wazero/internal/testing/require"
+	"github.com/tetratelabs/wazero/internal/wasm"
 	"github.com/tetratelabs/wazero/sys"
 )
 
 var hammers = map[string]testCase{
 	// Tests here are similar to what's described in /RATIONALE.md, but deviate as they involve blocking functions.
-	"close importing module while in use": {f: closeImportingModuleWhileInUse},
-	"close imported module while in use":  {f: closeImportedModuleWhileInUse},
+	"close importing module while in use":                 {f: closeImportingModuleWhileInUse},
+	"close imported module while in use":                  {f: closeImportedModuleWhileInUse},
+	"concurrent compilation, instantiation and execution": {f: concurrentCompilationInstantiationExecution},
 }
 
 func TestEngineCompiler_hammer(t *testing.T) {
@@ -28,6 +31,35 @@ func TestEngineCompiler_hammer(t *testing.T) {
 
 func TestEngineInterpreter_hammer(t *testing.T) {
 	runAllTests(t, hammers, wazero.NewRuntimeConfigInterpreter(), false)
+}
+
+func concurrentCompilationInstantiationExecution(t *testing.T, r wazero.Runtime) {
+	P := 16              // max count of goroutines
+	if testing.Short() { // Adjust down if `-test.short`
+		P = 4
+	}
+
+	hammer.NewHammer(t, P, 50).Run(func(p, n int) {
+		var body []byte
+		for i := 0; i < p*n; i++ {
+			body = append(body, wasm.OpcodeLocalGet, 0, wasm.OpcodeI32Const, 1, wasm.OpcodeI32Add, wasm.OpcodeLocalSet, 0)
+		}
+		body = append(body, wasm.OpcodeLocalGet, 0, wasm.OpcodeEnd)
+
+		bin := binaryencoding.EncodeModule(&wasm.Module{
+			TypeSection:     []wasm.FunctionType{{Results: []wasm.ValueType{i32}}},
+			FunctionSection: []wasm.Index{0},
+			CodeSection:     []wasm.Code{{LocalTypes: []wasm.ValueType{i32}, Body: body}},
+			ExportSection:   []wasm.Export{{Index: 0, Type: wasm.ExternTypeFunc, Name: "f"}},
+		})
+		m, err := r.Instantiate(testCtx, bin)
+		require.NoError(t, err)
+		fn := m.ExportedFunction("f")
+		require.NotNil(t, fn)
+		res, err := fn.Call(testCtx)
+		require.NoError(t, err)
+		require.Equal(t, uint64(p*n), res[0])
+	}, nil)
 }
 
 func closeImportingModuleWhileInUse(t *testing.T, r wazero.Runtime) {
@@ -99,7 +131,10 @@ func closeModuleWhileInUse(t *testing.T, r wazero.Runtime, closeFn func(imported
 	i := importing // pin the module used inside goroutines
 	hammer.NewHammer(t, P, 1).Run(func(p, n int) {
 		// In all cases, the importing module is closed, so the error should have that as its module name.
-		requireFunctionCallExits(t, i.ExportedFunction("call_return_input"))
+		fn := i.ExportedFunction("call_return_input")
+		require.NotNil(t, fn)
+		_, err := fn.Call(testCtx, 3)
+		require.Equal(t, sys.NewExitError(0), err)
 	}, func() { // When all functions are in-flight, re-assign the modules.
 		imported, importing = closeFn(imported, importing)
 		// Unblock all the calls
@@ -113,16 +148,9 @@ func closeModuleWhileInUse(t *testing.T, r wazero.Runtime, closeFn func(imported
 	}
 
 	// If unloading worked properly, a new function call should route to the newly instantiated module.
-	requireFunctionCall(t, importing.ExportedFunction("call_return_input"))
-}
-
-func requireFunctionCall(t *testing.T, fn api.Function) {
+	fn := importing.ExportedFunction("call_return_input")
+	require.NotNil(t, fn)
 	res, err := fn.Call(testCtx, 3)
 	require.NoError(t, err)
 	require.Equal(t, uint64(3), res[0])
-}
-
-func requireFunctionCallExits(t *testing.T, fn api.Function) {
-	_, err := fn.Call(testCtx, 3)
-	require.Equal(t, sys.NewExitError(0), err)
 }
