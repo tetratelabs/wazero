24cc2a61dad9899a367028527881ad686f9f8c71
Edoardo Vacchi <evacchi@users.noreply.github.com>
2024-11-23 00:38:59 +0100

    arm64: account for imported functions when relocation islands are encoded
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- internal/engine/wazevo/backend/isa/arm64/machine_relocation_test.go
+++ internal/engine/wazevo/backend/isa/arm64/machine_relocation_test.go
@@ -20,7 +20,8 @@ func Test_encodeCallTrampolineIsland(t *testing.T) {
 	executable := make([]byte, 16*1000)
 	islandOffset := 160
 	refToBinaryOffset := []int{0: 0, 1: 16, 2: 1600, 3: 16000}
-	encodeCallTrampolineIsland(refToBinaryOffset, islandOffset, executable)
+
+	encodeCallTrampolineIsland(refToBinaryOffset, 0, islandOffset, executable)
 	for i := 0; i < len(refToBinaryOffset); i++ {
 		offset := islandOffset + trampolineCallSize*i
 		instrs := executable[offset : offset+trampolineCallSize-4]
@@ -29,6 +30,23 @@ func Test_encodeCallTrampolineIsland(t *testing.T) {
 		imm := binary.LittleEndian.Uint32(executable[offset+trampolineCallSize-4:])
 		require.Equal(t, uint32(refToBinaryOffset[ssa.FuncRef(i)]-(offset+16)), imm)
 	}
+
+	executable = executable[:]
+	// We test again, assuming that the first offset is an imported function.
+	const importedFns = 1
+	encodeCallTrampolineIsland(refToBinaryOffset, importedFns, islandOffset, executable)
+	for i := 0; i < len(refToBinaryOffset[importedFns:]); i++ {
+		offset := islandOffset + trampolineCallSize*i
+		instrs := executable[offset : offset+trampolineCallSize-4]
+		// Instructions are always the same except for the last immediate.
+		require.Equal(t, "9b0000106b0380b97b030b8b60031fd6", hex.EncodeToString(instrs))
+		imm := binary.LittleEndian.Uint32(executable[offset+trampolineCallSize-4:])
+		require.Equal(t, uint32(refToBinaryOffset[ssa.FuncRef(i+importedFns)]-(offset+16)), imm)
+	}
+	// If the encoding is incorrect, then this offset should contain zeroes.
+	finalOffset := islandOffset + trampolineCallSize*len(refToBinaryOffset)
+	instrs := executable[finalOffset : finalOffset+trampolineCallSize]
+	require.Equal(t, "0000000000000000000000000000000000000000", hex.EncodeToString(instrs))
 }
 
 func Test_searchTrampolineIsland(t *testing.T) {
