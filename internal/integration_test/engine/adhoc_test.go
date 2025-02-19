a1347966afc9adf57c50e6444cf4a86426ed14fc
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-02-18 18:30:01 -0800

    Fixes testHostFunctionNumericParameter
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- internal/integration_test/engine/adhoc_test.go
+++ internal/integration_test/engine/adhoc_test.go
@@ -502,8 +502,14 @@ func testHostFunctionNumericParameter(t *testing.T, r wazero.Runtime) {
 		"i32": func(ctx context.Context, p uint32) uint32 {
 			return p + 1
 		},
+		"i32n": func(ctx context.Context, p int32) int32 {
+			return p - 1
+		},
 		"i64": func(ctx context.Context, p uint64) uint64 {
 			return p + 1
+		},
+		"i64n": func(ctx context.Context, p int64) int64 {
+			return p - 1
 		},
 		"f32": func(ctx context.Context, p float32) float32 {
 			return p + 1
@@ -525,10 +531,22 @@ func testHostFunctionNumericParameter(t *testing.T, r wazero.Runtime) {
 			expected: math.MaxUint32,
 		},
 		{
+			name:     "i32n",
+			vt:       i32,
+			input:    api.EncodeI32(math.MinInt32 + 1),
+			expected: api.EncodeI32(math.MinInt32),
+		},
+		{
 			name:     "i64",
 			vt:       i64,
 			input:    math.MaxUint64 - 1,
 			expected: math.MaxUint64,
+		},
+		{
+			name:     "i64n",
+			vt:       i64,
+			input:    api.EncodeI64(math.MinInt64 + 1),
+			expected: api.EncodeI64(math.MinInt64),
 		},
 		{
 			name:     "f32",
@@ -562,7 +580,12 @@ func testHostFunctionNumericParameter(t *testing.T, r wazero.Runtime) {
 
 			results, err := importing.ExportedFunction("call_return_input").Call(testCtx, test.input)
 			require.NoError(t, err)
-			require.Equal(t, test.expected, results[0])
+			switch test.vt {
+			case i32, f32:
+				require.Equal(t, uint32(test.expected), uint32(results[0]))
+			case i64, f64:
+				require.Equal(t, test.expected, results[0])
+			}
 		})
 	}
 }
